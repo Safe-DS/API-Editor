347fc1c4df49fddfefbd0f999025b9f60f520df3
Lars Reimann <mail@larsreimann.com>
2021-04-15 14:00:58 +0200

    Add react-bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Alert from 'react-bootstrap/Alert';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Alert variant="info">You can use all components from <a href="https://react-bootstrap.github.io/components/alerts/">React-Bootstrap.</a></Alert>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
