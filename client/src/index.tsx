b720c8d8853432be1a4900abf209990d187ecaee
Bogdan Deynega <s6bodeyn@uni-bonn.de>
2021-04-30 11:16:08 +0200

    Fixing File structure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from "./Components/App/App";
 
 ReactDOM.render(
   <React.StrictMode>
