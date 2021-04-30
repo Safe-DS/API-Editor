99d117b27093e2ba026936fe6d150be9c33b0929
PaulV <s6pavoel@uni-bonn.de>
2021-04-30 11:44:32 +0200

    adds interactivity for module nodes

--- client/src/Components/Tree/ModuleNode.js
+++ client/src/Components/Tree/ModuleNode.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import ClassNode from "./ClassNode";
 import FunctionNode from "./FunctionNode";
 
 const ModuleNode = ({inputModule}) => {
+    const [childVisible, setChildVisibility] = useState(false);
+
+    let hasClasses = !!inputModule.classes && inputModule.classes.length !== 0;
+    let hasFunctions = !!inputModule.functions && inputModule.functions.length !== 0;
+
     return (
         <li key={inputModule.name}>
-            <div>
+            <div onClick={() => setChildVisibility((v) => !v)}>
                 {inputModule.name}
             </div>
-            <ul>
-                {inputModule.classes.map(moduleClass => (
-                    <ClassNode inputClass={moduleClass}/>
-                ))}
-            </ul>
-            <ul>
-                {inputModule.functions.map(moduleFunction => (
-                    <FunctionNode inputFunction={moduleFunction}/>
-                ))}
-            </ul>
+            <div>
+                {
+                    hasClasses && childVisible &&
+                        <ul>
+                            {inputModule.classes.map(moduleClass => (
+                                <ClassNode inputClass={moduleClass}/>
+                            ))}
+                        </ul>
+                }
+                {
+                    hasFunctions && childVisible &&
+                    <ul>
+                        {inputModule.functions.map(moduleFunction => (
+                            <FunctionNode inputFunction={moduleFunction}/>
+                        ))}
+                    </ul>
+                }
+            </div>
         </li>
     )
 };
