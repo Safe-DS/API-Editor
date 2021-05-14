a3f1acc3db8ed5cb50148b6a03508c70a9ad7846
trubizki <s6mitrub@uni-bonn.de>
2021-05-14 14:13:53 +0200

    small refactoring

--- client/src/Components/ParameterView/ParameterView.js
+++ client/src/Components/ParameterView/ParameterView.js
@@ -1,21 +1,15 @@
 import React from "react";
 
 const ParameterView = ({parameters}) => {
-    let hasName = !!parameters; //&& parameters.name.length !== 0;
 
     return (
-
-            <div>
-                {hasName &&
-
-
-                    parameters.map(function (parameters) {
-                        return (<p key={parameters.name}> {parameters.name}: {parameters.type} </p>)
-                    })
-                }
-
-            </div>
-
+        <div>
+            {
+                parameters?.map(function (parameters) {
+                    return (<p key={parameters.name}> {parameters.name}: {parameters.type} </p>)
+                })
+            }
+        </div>
     )
 };
 
