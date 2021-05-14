81ef74c644f9969fb3e540c290d624d8d36b0f26
Lars Reimann <mail@larsreimann.com>
2021-05-14 14:54:39 +0200

    Remove an old prop

--- client/src/Components/App/App.tsx
+++ client/src/Components/App/App.tsx
@@ -9,7 +9,7 @@ function App() {
     return (
         <div className="App">
 
-            <TreeView setParameters={setParameters}/>
+            <TreeView />
             <ParameterView parameters={parameters}/>
         </div>
     );
