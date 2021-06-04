267273b833689482b53c2eaf1c8811b950361a6d
PaulV <s6pavoel@uni-bonn.de>
2021-06-04 11:24:41 +0200

    resolved merge conflicts

--- client/src/Components/App/App.tsx
+++ client/src/Components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import ParameterView from "../ParameterView/ParameterView";
 import TreeView from "../TreeView/TreeView";
@@ -8,6 +8,7 @@ function App() {
 
     return (
         <div className="App">
+
             <TreeView setParameters={setParameters}/>
             <ParameterView inputParameters={parameters}/>
         </div>
