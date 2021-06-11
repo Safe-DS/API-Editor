61d653159d22fb0233fca40fc6d8e52b22c1dd78
trubizki <s6mitrub@uni-bonn.de>
2021-06-11 15:42:34 +0200

    "No Parameters available" Text wenn keine Parameter zur funktion existieren. Und bisschen css

--- client/src/Components/ParameterView/ParameterView.tsx
+++ client/src/Components/ParameterView/ParameterView.tsx
@@ -6,14 +6,21 @@ type ParameterViewProps = {inputParameters: PythonParameter[]}
 
 const ParameterView = ({inputParameters}: ParameterViewProps) => {
 
+    const hasInputParameters = inputParameters.length > 0;
+
     return (
 
         <div className="parameterViewDiv">
             <h2>Parameters</h2>
             {
+                hasInputParameters &&
                 inputParameters?.map(function (parameters) {
                     return (<ParameterNode key={parameters.name} inputParameter={parameters} />)
                 })
+            }
+            {
+                !hasInputParameters &&
+                    <h5>No Parameters available</h5>
             }
 
         </div>
