ad99a4c72e9e5a2033100cf050e68d222674bbe8
Lars Reimann <mail@larsreimann.com>
2021-06-18 11:50:25 +0200

    Fix build warnings

--- client/src/Components/ParameterView/ParameterView.tsx
+++ client/src/Components/ParameterView/ParameterView.tsx
@@ -8,7 +8,7 @@ type ParameterViewProps = {
     setSelection: any
 };
 
-const ParameterView = ({inputParameters, selection, setSelection }: ParameterViewProps) => {
+const ParameterView = ({inputParameters, selection }: ParameterViewProps) => {
 
     const hasInputParameters = inputParameters.length > 0;
     console.log(selection);
@@ -17,6 +17,7 @@ const ParameterView = ({inputParameters, selection, setSelection }: ParameterVie
         <div className="parameter-view">
             <div className="parameter-view-path" >
                 { selection.length > 0 ?
+                    // eslint-disable-next-line
                     selection.map<React.ReactNode>(n => <a href="#">{n}</a>)
                              .reduce((p, c) => [p, (<span> / </span>), c]) :
                     "" }
