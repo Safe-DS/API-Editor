219c6cae47a273536c85c8f7eb0ec110a965aba5
Leon Schmitz <leonschmitz@posteo.de>
2021-06-25 16:13:32 +0200

    Synchronisiere Parameter-View und Breadcrump mit Auswahl in Tree-View.

--- client/src/Components/Tree/ClassNode.tsx
+++ client/src/Components/Tree/ClassNode.tsx
@@ -3,52 +3,43 @@ import PythonClass from "../../model/PythonClass";
 import classNames from "classnames";
 import FunctionNode from "./FunctionNode";
 import {isEmptyList} from "../../util/listOperations";
-import PythonFunction from "../../model/PythonFunction";
-import PythonParameter from "../../model/PythonParameter";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faChalkboard} from "@fortawesome/free-solid-svg-icons";
 import VisibilityIndicator from "../Util/VisibilityIndicator";
+import PythonDeclaration from "../../model/PythonDeclaration";
 
 type ClassNodeProps = {
-    parentPath: string[],
     pythonClass: PythonClass,
-    selection: string[],
-    setSelection: (newValue: string[]) => void,
+    selection: PythonDeclaration,
+    setSelection: Setter<PythonDeclaration>,
     moduleName: string,
-    setParameters: Setter<PythonParameter[]>,
-    setSelectedFunction: Setter<Nullable<PythonFunction>>
-}
+};
 
-export default function ClassNode({
-                                      parentPath,
-                                      pythonClass,
-                                      selection,
-                                      setSelection,
-                                      setParameters,
-                                      setSelectedFunction
-                                  }: ClassNodeProps): JSX.Element {
+export default function ClassNode({pythonClass,
+                                   selection,
+                                   setSelection}: ClassNodeProps): JSX.Element {
 
     const [childVisible, setChildVisibility] = useState(false);
     const hasMethods = !isEmptyList(pythonClass.methods);
-    const path = parentPath.concat(pythonClass.name);
     const cssClasses = classNames(
         "tree-view-row", {
             "text-muted": !hasMethods,
             "cursor-na": !hasMethods,
             "pl-3-5rem": !hasMethods,
             "pl-3rem": hasMethods,
-            "selected": (selection.join() === path.join()) && hasMethods,
+            "selected": (selection.path().join() === pythonClass.path().join()) && hasMethods
         }
     );
+
+    const handleClick = function () {
+        setSelection(pythonClass);
+        setChildVisibility(!childVisible);
+    };
 
     return (
         <div className="class-node">
             <div className={cssClasses}
-                 onClick={() => {
-                     setSelection(path);
-                     setChildVisibility(!childVisible);
-                     setSelectedFunction(null);
-                 }}>
+                 onClick={handleClick}>
                 <VisibilityIndicator hasChildren={hasMethods} childrenVisible={childVisible}/>
                 <FontAwesomeIcon icon={faChalkboard}/>
                 {" "}
@@ -56,15 +47,11 @@ export default function ClassNode({
             </div>
             {hasMethods && childVisible && <div>
                 {pythonClass.methods.map(method => (
-                    <FunctionNode parentPath={path}
-                                  key={method.name}
+                    <FunctionNode key={method.name}
                                   pythonFunction={method}
                                   selection={selection}
                                   setSelection={setSelection}
-                                  isMethod={true}
-                                  setParameters={setParameters}
-                                  setSelectedFunction={setSelectedFunction}
-                    />
+                                  isMethod={true}/>
                 ))}
             </div>}
         </div>
--- client/src/Components/TreeView/TreeView.tsx
+++ client/src/Components/TreeView/TreeView.tsx
@@ -1,34 +1,23 @@
 import React from 'react';
 import Tree from "../Tree/Tree";
 import './tree-view.css';
-import packageJson from "../../data/sklearn.json";
-import {parsePythonPackageJson, PythonPackageJson} from "../../model/PythonPackageBuilder";
-import PythonFunction from "../../model/PythonFunction";
-import PythonParameter from "../../model/PythonParameter";
+import PythonPackage from "../../model/PythonPackage";
+import PythonDeclaration from "../../model/PythonDeclaration";
 
 type TreeViewProps = {
-    setParameters: Setter<PythonParameter[]>,
-    selection: string[],
-    setSelection: Setter<string[]>,
-    setSelectedFunction: Setter<Nullable<PythonFunction>>
+    pythonPackage: PythonPackage,
+    selection: PythonDeclaration,
+    setSelection: Setter<PythonDeclaration>
 }
 
-export default function TreeView({
-                                     setParameters,
-                                     selection,
-                                     setSelection,
-                                     setSelectedFunction
-                                 }: TreeViewProps): JSX.Element {
-    const pythonPackage = parsePythonPackageJson(packageJson as PythonPackageJson);
+export default function TreeView({pythonPackage, selection, setSelection}: TreeViewProps): JSX.Element {
+
     return (
         <div className="tree-view">
             <h2 className="package-name">{pythonPackage.name}</h2>
             <Tree pythonPackage={pythonPackage}
-                  setParameters={setParameters}
                   selection={selection}
-                  setSelection={setSelection}
-                  setSelectedFunction={setSelectedFunction}
-            />
+                  setSelection={setSelection}/>
         </div>
     );
 }
