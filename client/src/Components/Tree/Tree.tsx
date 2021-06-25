6ab50f980cc1298c35c65efbbd86e229fc42cd9e
Lars Reimann <mail@larsreimann.com>
2021-06-25 21:15:16 +0200

    Filter empty modules.
    For now, we still keep the extra variable "hasChildren" in ModuleNode (even though it is now always true) as a safeguard in case we want to change the filter behavior again. Since, if we show modules without classes or functions in the tree view, they should be grayed out.

--- client/src/Components/Tree/Tree.tsx
+++ client/src/Components/Tree/Tree.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PythonDeclaration from "../../model/PythonDeclaration";
 import PythonPackage from "../../model/PythonPackage";
+import {isEmptyList} from "../../util/listOperations";
 import {Setter} from "../../util/types";
 import ModuleNode from "./ModuleNode";
 import TreeCSS from "./Tree.module.css";
@@ -14,12 +15,14 @@ interface TreeProps {
 export default function Tree(props: TreeProps): JSX.Element {
     return (
         <div className={TreeCSS.tree}>
-            {props.pythonPackage.modules.map(module => (
-                <ModuleNode key={module.name}
-                            pythonModule={module}
-                            selection={props.selection}
-                            setSelection={props.setSelection}/>
-            ))}
+            {props.pythonPackage.modules
+                .filter(module => !isEmptyList(module.classes) || !isEmptyList(module.functions))
+                .map(module => (
+                    <ModuleNode key={module.name}
+                                pythonModule={module}
+                                selection={props.selection}
+                                setSelection={props.setSelection}/>
+                ))}
         </div>
     );
 }
