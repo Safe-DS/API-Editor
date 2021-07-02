1561d0a80601b977ee7f9dd0085431a3ed6f2b88
Lars Reimann <mail@larsreimann.com>
2021-07-02 16:28:28 +0200

    Let the App component handle the global layout

--- client/src/Components/App/App.tsx
+++ client/src/Components/App/App.tsx
@@ -5,19 +5,25 @@ import {parsePythonPackageJson, PythonPackageJson} from "../../model/PythonPacka
 import Menu from "../Menu/Menu";
 import ParameterView from "../ParameterView/ParameterView";
 import TreeView from "../TreeView/TreeView";
-import './App.css';
+import AppCSS from './App.module.css';
 
 export default function App(): JSX.Element {
     const pythonPackage = parsePythonPackageJson(pythonPackageJson as PythonPackageJson);
     const [selection, setSelection] = useState<PythonDeclaration>(pythonPackage);
 
     return (
-        <div className="App">
-            <Menu selection={selection}/>
-            <TreeView pythonPackage={pythonPackage}
-                      selection={selection}
-                      setSelection={setSelection}/>
-            <ParameterView selection={selection}/>
+        <div className={AppCSS.app}>
+            <div className={AppCSS.menu}>
+                <Menu selection={selection}/>
+            </div>
+            <div className={AppCSS.leftPane}>
+                <TreeView pythonPackage={pythonPackage}
+                          selection={selection}
+                          setSelection={setSelection}/>
+            </div>
+            <div className={AppCSS.rightPane}>
+                <ParameterView selection={selection}/>
+            </div>
         </div>
     );
 }
