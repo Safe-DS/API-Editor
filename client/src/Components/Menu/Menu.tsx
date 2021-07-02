06a9a58e4edb62224ca36e63c19dd65ff6c87598
Lars Reimann <mail@larsreimann.com>
2021-07-02 12:17:36 +0200

    Add an exemplary menu bar

---
+++ client/src/Components/Menu/Menu.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+
+export default function Menu(): JSX.Element {
+    return (
+        <Navbar className="Menu" bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider/>
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
