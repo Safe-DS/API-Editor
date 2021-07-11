6e0353d76a7ca86d5e3dbe4366949e10316ec5b8
Lars Reimann <mail@larsreimann.com>
2021-07-11 10:56:47 +0000

    Fix: Lint errors

--- client/src/Components/Tree/TreeNode.tsx
+++ client/src/Components/Tree/TreeNode.tsx
@@ -1,13 +1,13 @@
-import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import classNames from "classnames";
-import React, {useState} from "react";
-import PythonDeclaration from "../../model/python/PythonDeclaration";
-import {ChildrenProp} from "../../util/types";
-import VisibilityIndicator from "../Util/VisibilityIndicator";
-import TreeNodeCSS from "./TreeNode.module.css";
-import {Link} from "react-router-dom";
-import {useLocation} from "react-router";
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import classNames from 'classnames'
+import React, { useState } from 'react'
+import PythonDeclaration from '../../model/python/PythonDeclaration'
+import { ChildrenProp } from '../../util/types'
+import VisibilityIndicator from '../Util/VisibilityIndicator'
+import TreeNodeCSS from './TreeNode.module.css'
+import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router'
 
 interface TreeNodeProps extends ChildrenProp {
     declaration: PythonDeclaration
@@ -17,21 +17,22 @@ interface TreeNodeProps extends ChildrenProp {
 }
 
 export default function TreeNode(props: TreeNodeProps): JSX.Element {
-    const [showChildren, setShowChildren] = useState(false);
+    const [showChildren, setShowChildren] = useState(false)
+    const currentPath = useLocation().pathname
 
     const className = classNames({
-        [TreeNodeCSS.selected]: isSelected(props.declaration),
-        "text-muted": !props.isWorthClicking
-    });
+        [TreeNodeCSS.selected]: isSelected(props.declaration, currentPath),
+        'text-muted': !props.isWorthClicking,
+    })
 
-    const level = levelOf(props.declaration);
+    const level = levelOf(props.declaration)
     const style = {
-        paddingLeft: (level === 0 ? '1rem' : `calc(0.5 * ${level} * (1.25em + 0.25rem) + 1rem)`)
-    };
+        paddingLeft: level === 0 ? '1rem' : `calc(0.5 * ${level} * (1.25em + 0.25rem) + 1rem) `,
+    }
 
     const handleClick = () => {
-        setShowChildren(prevState => !prevState);
-    };
+        setShowChildren((prevState) => !prevState)
+    }
 
     return (
         <div className={TreeNodeCSS.treeNode}>
@@ -40,27 +41,22 @@ export default function TreeNode(props: TreeNodeProps): JSX.Element {
                     className={TreeNodeCSS.icon}
                     hasChildren={props.isExpandable}
                     showChildren={showChildren}
-                    isSelected={isSelected(props.declaration)}
+                    isSelected={isSelected(props.declaration, currentPath)}
                 />
-                <FontAwesomeIcon
-                    className={TreeNodeCSS.icon}
-                    icon={props.icon}
-                    fixedWidth
-                />
-                <Link className={TreeNodeCSS.treeNodeLink}
-                      to={`/${props.declaration.path().join("/")}`}>{props.declaration.name}</Link>
+                <FontAwesomeIcon className={TreeNodeCSS.icon} icon={props.icon} fixedWidth />
+                <Link className={TreeNodeCSS.treeNodeLink} to={`/${props.declaration.path().join('/')}`}>
+                    {props.declaration.name}
+                </Link>
             </div>
-            <div className={TreeNodeCSS.children}>
-                {showChildren && props.children}
-            </div>
+            <div className={TreeNodeCSS.children}>{showChildren && props.children}</div>
         </div>
-    );
+    )
 }
 
 function levelOf(declaration: PythonDeclaration): number {
-    return declaration.path().length - 2;
+    return declaration.path().length - 2
 }
 
-function isSelected(declaration: PythonDeclaration): boolean {
-    return `/${declaration.path().join("/")}` === useLocation().pathname;
+function isSelected(declaration: PythonDeclaration, currentPath: string): boolean {
+    return `/${declaration.path().join('/')}` === currentPath
 }
