c8bc8a39704db0203177c9bd990a506531ed65d2
PaulV <s6pavoel@uni-bonn.de>
2021-07-12 20:02:40 +0200

    add dialogs for imports

---
+++ client/src/Components/Dialog/ImportPythonPackageDialog.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {Button, Form, Modal} from "react-bootstrap";
+import {Setter} from "../../util/types";
+import "../ParameterView/ParameterView.css";
+
+interface ImportPythonPackageDialogProps {
+    isVisible: boolean
+    setIsVisible: Setter<boolean>
+}
+
+export default function ImportPythonPackageDialog(props: ImportPythonPackageDialogProps): JSX.Element {
+
+    const close = () => {
+        props.setIsVisible(false);
+    };
+
+    const submit = () => {
+        console.log("TODO");
+    };
+
+    return (
+        <Modal
+            onHide={close}
+            show={props.isVisible}
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>Import annotation file</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <Form noValidate>
+                    <Modal.Body>
+                        <Form.Group>
+                            <Form.Label>
+                                Please upload a new annotation file.
+                            </Form.Label>
+                        </Form.Group>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={close}>
+                            Close
+                        </Button>
+                        <Button variant="primary" type="button" onClick={submit}>
+                            Submit
+                        </Button>
+                    </Modal.Footer>
+                </Form>
+            </Modal.Body>
+        </Modal>
+    );
+}
