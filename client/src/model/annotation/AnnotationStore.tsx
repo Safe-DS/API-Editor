b6dd2efddabe2b4dd1f2a95e3c68dfdb87b14ca4
PaulV <s6pavoel@uni-bonn.de>
2021-07-16 15:29:08 +0200

    Json Funktionalität im AnnotationStore hinzugefügt

--- client/src/model/annotation/AnnotationStore.tsx
+++ client/src/model/annotation/AnnotationStore.tsx
@@ -2,6 +2,13 @@ import {Map} from "immutable";
 import {Nullable} from "../../util/types";
 import PythonDeclaration from "../python/PythonDeclaration";
 
+/* TODO: Es fehlen die Enums. */
+
+interface annotationsJson {
+    renamings: Map<string, string>,
+    enums: Map<string, Map<string, string>>
+}
+
 export default class AnnotationStore {
     private readonly renamings: RenameAnnotationStore
 
@@ -21,8 +28,22 @@ export default class AnnotationStore {
         return new AnnotationStore(this.renamings.set(declaration.pathAsString(), newName));
     }
 
+    private setRenaming(path: string, newName: string) {
+        return new AnnotationStore(this.renamings.set(path, newName));
+    }
+
     removeRenamingFor(declaration: PythonDeclaration): AnnotationStore {
         return new AnnotationStore(this.renamings.remove(declaration.pathAsString()));
+    }
+
+    toJson(): string {
+        return JSON.stringify({"renamings": this.renamings});
+    }
+
+    fromJson(annotations: annotationsJson): void {
+        annotations.renamings.forEach(((value, key) => {
+            this.setRenaming(value, key);
+        }));
     }
 }
 
