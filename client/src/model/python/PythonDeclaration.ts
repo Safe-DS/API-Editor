a1556727c0e09db8e2d78684b2dfcda26981b1d8
trubizki <s6mitrub@uni-bonn.de>
2021-07-16 13:57:53 +0200

    minor changes

--- client/src/model/python/PythonDeclaration.ts
+++ client/src/model/python/PythonDeclaration.ts
@@ -1,6 +1,5 @@
 import {isEmptyList} from "../../util/listOperations";
 import {Nullable} from "../../util/types";
-import EnumPair from "../EnumPair";
 
 export default abstract class PythonDeclaration {
     abstract readonly name: string
@@ -36,43 +35,3 @@ export default abstract class PythonDeclaration {
         return this.children().find(it => it.name === head)?.getByRelativePath(tail) ?? null;
     }
 }
-
-
-/*import {isEmptyList} from "../../util/listOperations";
-import {Nullable} from "../../util/types";
-import EnumPair from "../EnumPair";
-
-export default abstract class EnumDeclaration {
-    abstract readonly enumName: string;
-    abstract readonly enumPairs: EnumPair[];
-
-    abstract parent(): Nullable<EnumDeclaration>
-
-    abstract children(): EnumDeclaration[]
-
-    path(): string[] {
-        let current: Nullable<EnumDeclaration> = this;
-        const result: string[] = [];
-
-        while (current !== null) {
-            result.unshift(current.enumName);
-            current = current.parent();
-        }
-
-        return result;
-    }
-
-    pathAsString(): string {
-        return this.path().join("/");
-    }
-
-    getByRelativePath(relativePath: string[]): Nullable<EnumDeclaration> {
-        if (isEmptyList(relativePath)) {
-            return this;
-        }
-
-        const [head, ...tail] = relativePath;
-        return this.children().find(it => it.enumName === head)?.getByRelativePath(tail) ?? null;
-    }
-}
-*/
