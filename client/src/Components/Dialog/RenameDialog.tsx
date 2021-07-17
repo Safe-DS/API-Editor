1ad18886891905f125fb2efc91df5347c3595d4e
Lars Reimann <mail@larsreimann.com>
2021-07-17 20:35:14 +0200

    feat(dialog): normalized styling and wording
    * Renamed "close" to "cancel" and change styling
    * Normalize casing
    * Change styling of enum dialog to match rename dialog

--- client/src/Components/Dialog/RenameDialog.tsx
+++ client/src/Components/Dialog/RenameDialog.tsx
@@ -62,8 +62,8 @@ export default function RenameDialog(props: RenameDialogProps): JSX.Element {
                         </Form.Group>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant="secondary" onClick={close}>
-                            Close
+                        <Button variant="danger" onClick={close}>
+                            Cancel
                         </Button>
                         <Button variant="primary" type="button" onClick={submit}>
                             Submit
