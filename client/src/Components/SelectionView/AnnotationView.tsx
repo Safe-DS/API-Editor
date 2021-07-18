418d627d5de2b508181f37e565f5b5adb9408f48
Leon Schmitz <leonschmitz@posteo.de>
2021-07-18 12:13:35 +0200

    Stelle Angelegte Annotationen schoener dar

--- client/src/Components/SelectionView/AnnotationView.tsx
+++ client/src/Components/SelectionView/AnnotationView.tsx
@@ -1,35 +1,36 @@
 import {faTrash, faWrench} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import React from "react";
-import {Button, ButtonGroup, Col, Row} from "react-bootstrap";
+import {Button, ButtonGroup, Card, Col, Row} from "react-bootstrap";
 import {Nullable, Setter} from "../../util/types";
+import "./AnnotationView.css";
 
-interface RenameAnnotationViewProps {
+interface AnnotationViewProps {
     newName: Nullable<string>,
     setNewName: Setter<Nullable<string>>,
     onRenameEdit: () => void,
 }
 
-const RenameAnnotationView: React.FC<RenameAnnotationViewProps> = (props) => {
+const AnnotationView: React.FC<AnnotationViewProps> = (props) => {
     const deleteRenameAnnotation = () => props.setNewName(null);
 
     if (props.newName !== null) {
         return (<div className={"annotation-list"}>
             <h4>Annotations</h4>
-            <Row>
-                <Col className="align-items-center" xs="auto">
-                    <code>{`@rename: ${props.newName}`}</code>
-                </Col>
-                <Col xs="auto">
+            <Card className="mb-2 w-fit-content" bg="light">
+                <Card.Body>
+                    <code className="pe-3">
+                        {`@rename: ${props.newName}`}
+                    </code>
                     <ButtonGroup>
                         <Button size="sm" onClick={props.onRenameEdit}><FontAwesomeIcon icon={faWrench}/></Button>
                         <Button size="sm" onClick={deleteRenameAnnotation}><FontAwesomeIcon icon={faTrash}/></Button>
                     </ButtonGroup>
-                </Col>
-            </Row>
+                </Card.Body>
+            </Card>
         </div>);
     }
     return <></>;
 };
 
-export default RenameAnnotationView;
+export default AnnotationView;
