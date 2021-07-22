349d712d8dbc1f0d74597f8a39f42c205900bd96
PaulV <s6pavoel@uni-bonn.de>
2021-07-22 19:05:28 +0200

    bug: import and export now don't wrap unnecessarily anymore

--- client/src/Components/Menu/Menu.tsx
+++ client/src/Components/Menu/Menu.tsx
@@ -32,7 +32,7 @@ export default function Menu(props: MenuProps): JSX.Element {
     };
 
     return (
-        <Navbar className={cssClasses} bg="light" expand="lg">
+        <Navbar className={cssClasses} bg="light" expand="md">
             <Navbar.Text>{
                 pathname.map((x, i) => (
                     <React.Fragment key={i}>
@@ -44,7 +44,7 @@ export default function Menu(props: MenuProps): JSX.Element {
                     </React.Fragment>
                 ))}
             </Navbar.Text>
-            <Nav>
+            <Nav navbar={true}>
                 <NavDropdown title="Import" id="import-dropdown" align="end">
                     <NavDropdown.Item onClick={openImportPythonPackageDialog}>Python Package</NavDropdown.Item>
                     <NavDropdown.Item onClick={openImportAnnotationFileDialog}>Annotation File</NavDropdown.Item>
