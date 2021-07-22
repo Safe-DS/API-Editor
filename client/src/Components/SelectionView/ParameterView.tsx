a78ebb480ac232bfad0f5dd4bc7af86af513a654
PaulV <s6pavoel@uni-bonn.de>
2021-07-22 15:24:01 +0200

    remove duplicate parameter description

--- client/src/Components/SelectionView/ParameterView.tsx
+++ client/src/Components/SelectionView/ParameterView.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import AnnotationStore from "../../model/annotation/AnnotationStore";
 import PythonParameter from "../../model/python/PythonParameter";
 import {Setter} from "../../util/types";
-import DocumentationText from "./DocumentationText";
 import ParameterNode from "./ParameterNode";
 import TitleValueViewPair from "./TitleValueViewPair";
 
@@ -18,7 +17,6 @@ export default function ParameterView(props: ParameterViewProps): JSX.Element {
             <ParameterNode isTitle={true} pythonParameter={props.pythonParameter}
                            setAnnotationStore={props.setAnnotationStore}
                            annotationStore={props.annotationStore}/>
-            <DocumentationText inputText={props.pythonParameter.description}/>
             {props.pythonParameter.hasDefault &&
             <TitleValueViewPair title="Default value" value={props.pythonParameter.defaultValue}/>}
             {props.pythonParameter.type &&
