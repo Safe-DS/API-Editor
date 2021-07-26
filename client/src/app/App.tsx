899fd0f5c9ee0d257ddb3ba0e1733a4fe0d04648
Lars Reimann <mail@larsreimann.com>
2021-07-26 13:56:03 +0200

    performance: fix app constantly reloading

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -29,7 +29,7 @@ const App: React.FC = () => {
         }
 
         getPythonPackageFromIndexedDB()
-    })
+    }, [])
 
     const annotationStore = useAppSelector((state) => state.annotations)
 
