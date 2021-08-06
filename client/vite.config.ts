405524a5d3358e96d28f45fb0c8c1dad12b8337c
Lars Reimann <mail@larsreimann.com>
2021-08-06 21:21:48 +0200

    chore: remove mdast-util-to-hast-backport

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,11 +5,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     base: '/',
     plugins: [reactRefresh()],
-    resolve: {
-        alias: {
-            'mdast-util-to-hast': 'mdast-util-to-hast-backport',
-        },
-    },
     server: {
         proxy: {
             '/api': 'http://localhost:4280',
