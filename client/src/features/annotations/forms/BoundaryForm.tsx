5a1db752caca96d64dfc02a536de5a8d6172d565
PaulV <s6pavoel@uni-bonn.de>
2021-10-29 18:19:15 +0200

    add unrestricted validation

--- client/src/features/annotations/forms/BoundaryForm.tsx
+++ client/src/features/annotations/forms/BoundaryForm.tsx
@@ -15,14 +15,20 @@ import {
     Stack,
     Text,
 } from '@chakra-ui/react'
-import React, {useEffect} from 'react'
-import {useForm} from 'react-hook-form'
-import {useAppDispatch, useAppSelector} from '../../../app/hooks'
-import {numberPattern} from '../../../common/validation'
+import React, { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useAppDispatch, useAppSelector } from '../../../app/hooks'
+import { numberPattern } from '../../../common/validation'
 import PythonDeclaration from '../../packageData/model/PythonDeclaration'
-import {ComparisonOperator, hideAnnotationForms, Interval, selectBoundary, upsertBoundary,} from '../annotationSlice'
+import {
+    ComparisonOperator,
+    hideAnnotationForms,
+    Interval,
+    selectBoundary,
+    upsertBoundary,
+} from '../annotationSlice'
 import AnnotationForm from './AnnotationForm'
-import {Optional} from "../../../common/util/types";
+import { Optional } from '../../../common/util/types'
 
 interface BoundaryFormProps {
     readonly target: PythonDeclaration;
@@ -38,7 +44,7 @@ interface BoundaryFormState {
     };
 }
 
-const initialFormState = function (previousInterval: Optional<Interval>): BoundaryFormState {
+const initialFormState = function(previousInterval: Optional<Interval>): BoundaryFormState {
     return {
         interval: {
             isDiscrete: previousInterval?.isDiscrete ?? false,
@@ -50,7 +56,7 @@ const initialFormState = function (previousInterval: Optional<Interval>): Bounda
     }
 }
 
-const BoundaryForm: React.FC<BoundaryFormProps> = function ({target}) {
+const BoundaryForm: React.FC<BoundaryFormProps> = function({ target }) {
     const targetPath = target.pathAsString()
     const prevInterval = useAppSelector(selectBoundary(targetPath))?.interval
 
@@ -64,10 +70,10 @@ const BoundaryForm: React.FC<BoundaryFormProps> = function ({target}) {
         getValues,
         watch,
         setValue,
-        formState: {errors},
+        formState: { errors },
     } = useForm<BoundaryFormState>({
         defaultValues: initialFormState(prevInterval),
-        shouldFocusError: false
+        shouldFocusError: false,
     })
 
     useEffect(() => {
@@ -89,7 +95,7 @@ const BoundaryForm: React.FC<BoundaryFormProps> = function ({target}) {
         dispatch(
             upsertBoundary({
                 target: targetPath,
-                interval: {...data.interval},
+                interval: { ...data.interval },
             }),
         )
         dispatch(hideAnnotationForms())
@@ -101,6 +107,13 @@ const BoundaryForm: React.FC<BoundaryFormProps> = function ({target}) {
 
 
     // Validation ------------------------------------------------------------------------------------------------------
+
+    const unrestrictedInterval = () => {
+        const lowerLimitType = getValues('interval.lowerLimitType')
+        const upperLimitType = getValues('interval.upperLimitType')
+
+        return !(lowerLimitType === ComparisonOperator.UNRESTRICTED && upperLimitType === ComparisonOperator.UNRESTRICTED)
+    }
 
     const nonEmptyInterval = () => {
         const lowerLimit = getValues('interval.lowIntervalLimit')
@@ -156,50 +169,84 @@ const BoundaryForm: React.FC<BoundaryFormProps> = function ({target}) {
                             valueAsNumber: true,
                             disabled: watch('interval.lowerLimitType') === ComparisonOperator.UNRESTRICTED,
                             validate: {
-                                nonEmptyInterval
-                            }
+                                nonEmptyInterval,
+                            },
                         })}
                         onChange={(_, valueAsNumber) => setValue('interval.lowIntervalLimit', valueAsNumber)}
                     >
-                        <NumberInputField/>
+                        <NumberInputField />
                         <NumberInputStepper>
-                            <NumberIncrementStepper/>
-                            <NumberDecrementStepper/>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
                         </NumberInputStepper>
                     </NumberInput>
                     <FormErrorMessage>
-                        <FormErrorIcon/>
+                        <FormErrorIcon />
                         {
                             errors?.interval?.lowIntervalLimit?.message ||
-                            errors?.interval?.lowIntervalLimit?.type === "nonEmptyInterval" && "Interval is empty."
+                            errors?.interval?.lowIntervalLimit?.type === 'nonEmptyInterval' && 'Interval is empty.'
                         }
                     </FormErrorMessage>
                 </FormControl>
-                <Select
-                    {...register('interval.lowerLimitType', {
-                        required: 'This is required.',
-                        valueAsNumber: true
-                    })}
-                >
-                    <option value={ComparisonOperator.LESS_THAN_OR_EQUALS}>≤</option>
-                    <option value={ComparisonOperator.LESS_THAN}>{'<'}</option>
-                    <option value={ComparisonOperator.UNRESTRICTED}>no lower limit</option>
-                </Select>
+                <FormControl>
+                    <Select
+                        {...register('interval.lowerLimitType', {
+                            required: 'This is required.',
+                            valueAsNumber: true,
+                            validate: {
+                                unrestrictedInterval,
+                            },
+                        })}
+                    >
+                        <option
+                            value={ComparisonOperator.LESS_THAN_OR_EQUALS}>≤
+                        </option>
+                        <option
+                            value={ComparisonOperator.LESS_THAN}>{'<'}</option>
+                        <option value={ComparisonOperator.UNRESTRICTED}>no lower
+                            limit
+                        </option>
+                    </Select>
+                    <FormErrorMessage>
+                        <FormErrorIcon />
+                        {
+                            errors?.interval?.lowerLimitType?.message ||
+                            errors?.interval?.lowerLimitType?.type === 'unrestrictedInterval' && 'Interval is unrestricted.'
+                        }
+                    </FormErrorMessage>
+                </FormControl>
 
                 <Text fontWeight='bold' paddingTop={2}>
                     {target.name}
                 </Text>
 
-                <Select
-                    {...register('interval.upperLimitType', {
-                        required: 'This is required.',
-                        valueAsNumber: true,
-                    })}
-                >
-                    <option value={ComparisonOperator.LESS_THAN_OR_EQUALS}>≤</option>
-                    <option value={ComparisonOperator.LESS_THAN}>{'<'}</option>
-                    <option value={ComparisonOperator.UNRESTRICTED}>no upper limit</option>
-                </Select>
+                <FormControl>
+                    <Select
+                        {...register('interval.upperLimitType', {
+                            required: 'This is required.',
+                            valueAsNumber: true,
+                            validate: {
+                                unrestrictedInterval,
+                            },
+                        })}
+                    >
+                        <option
+                            value={ComparisonOperator.LESS_THAN_OR_EQUALS}>≤
+                        </option>
+                        <option
+                            value={ComparisonOperator.LESS_THAN}>{'<'}</option>
+                        <option value={ComparisonOperator.UNRESTRICTED}>no upper
+                            limit
+                        </option>
+                    </Select>
+                    <FormErrorMessage>
+                        <FormErrorIcon />
+                        {
+                            errors?.interval?.upperLimitType?.message ||
+                            errors?.interval?.upperLimitType?.type === 'unrestrictedInterval' && 'Interval is unrestricted.'
+                        }
+                    </FormErrorMessage>
+                </FormControl>
                 <FormControl
                     isInvalid={Boolean(errors?.interval?.upperIntervalLimit)}>
                     <NumberInput
@@ -209,22 +256,22 @@ const BoundaryForm: React.FC<BoundaryFormProps> = function ({target}) {
                             valueAsNumber: true,
                             disabled: watch('interval.upperLimitType') === ComparisonOperator.UNRESTRICTED,
                             validate: {
-                                nonEmptyInterval
-                            }
+                                nonEmptyInterval,
+                            },
                         })}
                         onChange={(_, valueAsNumber) => setValue('interval.upperIntervalLimit', valueAsNumber)}
                     >
-                        <NumberInputField/>
+                        <NumberInputField />
                         <NumberInputStepper>
-                            <NumberIncrementStepper/>
-                            <NumberDecrementStepper/>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
                         </NumberInputStepper>
                     </NumberInput>
                     <FormErrorMessage>
-                        <FormErrorIcon/>
+                        <FormErrorIcon />
                         {
                             errors?.interval?.upperIntervalLimit?.message ||
-                            errors?.interval?.upperIntervalLimit?.type === "nonEmptyInterval" && "Interval is empty."
+                            errors?.interval?.upperIntervalLimit?.type === 'nonEmptyInterval' && 'Interval is empty.'
                         }
                     </FormErrorMessage>
                 </FormControl>
