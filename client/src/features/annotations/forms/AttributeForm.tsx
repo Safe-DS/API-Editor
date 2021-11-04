992fb842951f391023157433f5d0bd809057d3a9
Lars Reimann <mail@larsreimann.com>
2021-11-04 15:30:14 +0100

    refactor: extract common parts from @attribute, @constant, and @optional annotations

--- client/src/features/annotations/forms/AttributeForm.tsx
+++ client/src/features/annotations/forms/AttributeForm.tsx
@@ -1,163 +1,43 @@
-import {
-    FormControl,
-    FormErrorIcon,
-    FormErrorMessage,
-    FormLabel,
-    Input,
-    NumberDecrementStepper,
-    NumberIncrementStepper,
-    NumberInput,
-    NumberInputField,
-    NumberInputStepper,
-    Radio,
-    RadioGroup,
-    Select,
-    Stack,
-} from '@chakra-ui/react';
-import React, { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
-import { booleanPattern, numberPattern } from '../../../common/validation';
 import PythonDeclaration from '../../packageData/model/PythonDeclaration';
-import {
-    DefaultType,
-    DefaultValue,
-    hideAnnotationForms,
-    selectAttribute,
-    upsertAttribute,
-} from '../annotationSlice';
-import AnnotationForm from './AnnotationForm';
+import { selectAttribute, upsertAttribute } from '../annotationSlice';
+import TypeValueForm, { TypeValueFormState } from './TypeValueForm';
 
 interface AttributeFormProps {
     target: PythonDeclaration;
-}
-
-interface AttributeFormState {
-    defaultValue: DefaultValue;
-    defaultType: DefaultType;
 }
 
 const AttributeForm: React.FC<AttributeFormProps> = function ({ target }) {
     const targetPath = target.pathAsString();
 
     // Hooks -----------------------------------------------------------------------------------------------------------
-    const attributeDefinition = useAppSelector(selectAttribute(targetPath));
-    const prevDefaultType = attributeDefinition?.defaultType;
-    const prevDefaultValue = attributeDefinition?.defaultValue;
+    const previousAnnotation = useAppSelector(selectAttribute(targetPath));
+    const previousDefaultType = previousAnnotation?.defaultType;
+    const previousDefaultValue = previousAnnotation?.defaultValue;
     const dispatch = useAppDispatch();
-
-    const {
-        handleSubmit,
-        register,
-        reset,
-        setValue,
-        watch,
-        formState: { errors },
-    } = useForm<AttributeFormState>({
-        defaultValues: {
-            defaultType: 'string',
-            defaultValue: '',
-        },
-    });
-
-    const watchDefaultType = watch('defaultType');
-
-    useEffect(() => {
-        reset({
-            defaultType: prevDefaultType || 'string',
-            defaultValue: prevDefaultValue || '',
-        });
-    }, [reset, prevDefaultType, prevDefaultValue]);
 
     // Event handlers --------------------------------------------------------------------------------------------------
 
-    const handleTypeChange = (newType: DefaultType) => {
-        setValue('defaultType', newType);
-        reset({
-            defaultType: newType,
-            defaultValue: '',
-        });
-    };
-
-    const onSave = (data: AttributeFormState) => {
+    const handleUpsertAnnotation = (data: TypeValueFormState) => {
         dispatch(
             upsertAttribute({
                 target: targetPath,
                 ...data,
             }),
         );
-        dispatch(hideAnnotationForms());
-    };
-
-    const onCancel = () => {
-        dispatch(hideAnnotationForms());
     };
 
     // Rendering -------------------------------------------------------------------------------------------------------
 
     return (
-        <AnnotationForm
-            heading={`${
-                attributeDefinition ? 'Edit' : 'Add'
-            } @attribute annotation`}
-            onSave={handleSubmit(onSave)}
-            onCancel={onCancel}
-        >
-            <FormLabel>
-                Type of default value of &quot;{target.name}&quot;:
-            </FormLabel>
-            <RadioGroup
-                defaultValue={prevDefaultType || 'string'}
-                onChange={handleTypeChange}
-            >
-                <Stack direction="column">
-                    <Radio value="string">String</Radio>
-                    <Radio value="number">Number</Radio>
-                    <Radio value="boolean">Boolean</Radio>
-                </Stack>
-            </RadioGroup>
-
-            <FormControl isInvalid={Boolean(errors?.defaultValue)}>
-                <FormLabel>
-                    Default value for &quot;{target.name}&quot;:
-                </FormLabel>
-                {watchDefaultType === 'string' && (
-                    <Input
-                        {...register('defaultValue', {
-                            required: 'This is required.',
-                        })}
-                    />
-                )}
-                {watchDefaultType === 'number' && (
-                    <NumberInput>
-                        <NumberInputField
-                            {...register('defaultValue', {
-                                required: 'This is required.',
-                                pattern: numberPattern,
-                            })}
-                        />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
-                )}
-                {watchDefaultType === 'boolean' && (
-                    <Select
-                        {...register('defaultValue', {
-                            required: 'This is required.',
-                            pattern: booleanPattern,
-                        })}
-                    >
-                        <option value="true">True</option>
-                        <option value="false">False</option>
-                    </Select>
-                )}
-                <FormErrorMessage>
-                    <FormErrorIcon /> {errors.defaultValue?.message}
-                </FormErrorMessage>
-            </FormControl>
-        </AnnotationForm>
+        <TypeValueForm
+            target={target}
+            annotationType="attribute"
+            previousDefaultType={previousDefaultType}
+            previousDefaultValue={previousDefaultValue}
+            onUpsertAnnotation={handleUpsertAnnotation}
+        />
     );
 };
 
--- client/src/features/annotations/forms/ConstantForm.tsx
+++ client/src/features/annotations/forms/ConstantForm.tsx
@@ -1,40 +1,11 @@
-import {
-    FormControl,
-    FormErrorIcon,
-    FormErrorMessage,
-    FormLabel,
-    Input,
-    NumberDecrementStepper,
-    NumberIncrementStepper,
-    NumberInput,
-    NumberInputField,
-    NumberInputStepper,
-    Radio,
-    RadioGroup,
-    Select,
-    Stack,
-} from '@chakra-ui/react';
-import React, { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
-import { booleanPattern, numberPattern } from '../../../common/validation';
 import PythonDeclaration from '../../packageData/model/PythonDeclaration';
-import {
-    DefaultType,
-    DefaultValue,
-    hideAnnotationForms,
-    selectConstant,
-    upsertConstant,
-} from '../annotationSlice';
-import AnnotationForm from './AnnotationForm';
+import { selectConstant, upsertConstant } from '../annotationSlice';
+import TypeValueForm, { TypeValueFormState } from './TypeValueForm';
 
 interface ConstantFormProps {
     target: PythonDeclaration;
-}
-
-interface ConstantFormState {
-    defaultValue: DefaultValue;
-    defaultType: DefaultType;
 }
 
 const ConstantForm: React.FC<ConstantFormProps> = function ({ target }) {
@@ -42,122 +13,31 @@ const ConstantForm: React.FC<ConstantFormProps> = function ({ target }) {
 
     // Hooks -----------------------------------------------------------------------------------------------------------
     const constantDefinition = useAppSelector(selectConstant(targetPath));
-    const prevDefaultType = constantDefinition?.defaultType;
-    const prevDefaultValue = constantDefinition?.defaultValue;
+    const previousDefaultType = constantDefinition?.defaultType;
+    const previousDefaultValue = constantDefinition?.defaultValue;
     const dispatch = useAppDispatch();
-
-    const {
-        handleSubmit,
-        register,
-        reset,
-        setValue,
-        watch,
-        formState: { errors },
-    } = useForm<ConstantFormState>({
-        defaultValues: {
-            defaultType: 'string',
-            defaultValue: '',
-        },
-    });
-
-    const watchDefaultType = watch('defaultType');
-
-    useEffect(() => {
-        reset({
-            defaultType: prevDefaultType || 'string',
-            defaultValue: prevDefaultValue || '',
-        });
-    }, [reset, prevDefaultType, prevDefaultValue]);
 
     // Event handlers --------------------------------------------------------------------------------------------------
 
-    const handleTypeChange = (newType: DefaultType) => {
-        setValue('defaultType', newType);
-        reset({
-            defaultType: newType,
-            defaultValue: '',
-        });
-    };
-
-    const onSave = (data: ConstantFormState) => {
+    const handleUpsertAnnotation = (data: TypeValueFormState) => {
         dispatch(
             upsertConstant({
                 target: targetPath,
                 ...data,
             }),
         );
-        dispatch(hideAnnotationForms());
-    };
-
-    const onCancel = () => {
-        dispatch(hideAnnotationForms());
     };
 
     // Rendering -------------------------------------------------------------------------------------------------------
 
     return (
-        <AnnotationForm
-            heading={`${
-                constantDefinition ? 'Edit' : 'Add'
-            } @constant annotation`}
-            onSave={handleSubmit(onSave)}
-            onCancel={onCancel}
-        >
-            <FormLabel>
-                Type of default value of &quot;{target.name}&quot;:
-            </FormLabel>
-            <RadioGroup
-                defaultValue={prevDefaultType || 'string'}
-                onChange={handleTypeChange}
-            >
-                <Stack direction="column">
-                    <Radio value="string">String</Radio>
-                    <Radio value="number">Number</Radio>
-                    <Radio value="boolean">Boolean</Radio>
-                </Stack>
-            </RadioGroup>
-
-            <FormControl isInvalid={Boolean(errors?.defaultValue)}>
-                <FormLabel>
-                    Default value for &quot;{target.name}&quot;:
-                </FormLabel>
-                {watchDefaultType === 'string' && (
-                    <Input
-                        {...register('defaultValue', {
-                            required: 'This is required.',
-                        })}
-                    />
-                )}
-                {watchDefaultType === 'number' && (
-                    <NumberInput>
-                        <NumberInputField
-                            {...register('defaultValue', {
-                                required: 'This is required.',
-                                pattern: numberPattern,
-                            })}
-                        />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
-                )}
-                {watchDefaultType === 'boolean' && (
-                    <Select
-                        {...register('defaultValue', {
-                            required: 'This is required.',
-                            pattern: booleanPattern,
-                        })}
-                    >
-                        <option value="true">True</option>
-                        <option value="false">False</option>
-                    </Select>
-                )}
-                <FormErrorMessage>
-                    <FormErrorIcon /> {errors.defaultValue?.message}
-                </FormErrorMessage>
-            </FormControl>
-        </AnnotationForm>
+        <TypeValueForm
+            target={target}
+            annotationType="constant"
+            previousDefaultType={previousDefaultType}
+            previousDefaultValue={previousDefaultValue}
+            onUpsertAnnotation={handleUpsertAnnotation}
+        />
     );
 };
 
