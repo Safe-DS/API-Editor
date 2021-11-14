57ad32d3e3edc94b07f70763dc21cf83bafc0023
Lars Reimann <mail@larsreimann.com>
2021-11-14 11:48:56 +0100

    refactor: rename variable

--- server/src/main/java/com/larsreimann/api_editor/server/validation/AnnotationTargetError.java
+++ server/src/main/java/com/larsreimann/api_editor/server/validation/AnnotationTargetError.java
@@ -5,17 +5,17 @@ import com.larsreimann.api_editor.server.data.AnnotationTarget;
 public class AnnotationTargetError implements AnnotationError {
     private final String qualifiedName;
     private final String annotationName;
-    private final AnnotationTarget type;
+    private final AnnotationTarget target;
 
-    public AnnotationTargetError(String qualifiedName, String annotationName, AnnotationTarget type) {
+    public AnnotationTargetError(String qualifiedName, String annotationName, AnnotationTarget target) {
         this.qualifiedName = qualifiedName;
         this.annotationName = annotationName;
-        this.type = type;
+        this.target = target;
     }
 
     @Override
     public String message() {
         return "The annotation " + annotationName + " cannot be set for the following element of type "
-            + type.toString() + ": " + qualifiedName;
+            + target.toString() + ": " + qualifiedName;
     }
 }
