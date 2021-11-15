f2ec2504c08bd9c4475a8c30ec243ca0fe42bb57
Lars Reimann <mail@larsreimann.com>
2021-11-15 12:40:48 +0100

    refactor: turn annotation errors into records

--- server/src/main/java/com/larsreimann/api_editor/server/validation/AnnotationCombinationError.java
+++ server/src/main/java/com/larsreimann/api_editor/server/validation/AnnotationCombinationError.java
@@ -1,15 +1,10 @@
 package com.larsreimann.api_editor.server.validation;
 
-public class AnnotationCombinationError implements AnnotationError{
-    private final String qualifiedName;
-    private final String firstAnnotationName;
-    private final String secondAnnotationName;
-
-    public AnnotationCombinationError(String qualifiedName, String firstAnnotationName, String secondAnnotationName) {
-        this.qualifiedName = qualifiedName;
-        this.firstAnnotationName = firstAnnotationName;
-        this.secondAnnotationName = secondAnnotationName;
-    }
+public record AnnotationCombinationError(
+    String qualifiedName,
+    String firstAnnotationName,
+    String secondAnnotationName
+) implements AnnotationError {
 
     @Override
     public String message() {
--- server/src/main/java/com/larsreimann/api_editor/server/validation/AnnotationTargetError.java
+++ server/src/main/java/com/larsreimann/api_editor/server/validation/AnnotationTargetError.java
@@ -2,16 +2,11 @@ package com.larsreimann.api_editor.server.validation;
 
 import com.larsreimann.api_editor.server.data.AnnotationTarget;
 
-public class AnnotationTargetError implements AnnotationError {
-    private final String qualifiedName;
-    private final String annotationName;
-    private final AnnotationTarget target;
-
-    public AnnotationTargetError(String qualifiedName, String annotationName, AnnotationTarget target) {
-        this.qualifiedName = qualifiedName;
-        this.annotationName = annotationName;
-        this.target = target;
-    }
+public record AnnotationTargetError(
+    String qualifiedName,
+    String annotationName,
+    AnnotationTarget target
+) implements AnnotationError {
 
     @Override
     public String message() {
