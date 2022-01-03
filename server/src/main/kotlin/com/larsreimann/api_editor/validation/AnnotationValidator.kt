8e315f243a2b7a1772ebfe30f09f3422cb2318fc
Lars Reimann <mail@larsreimann.com>
2022-01-03 14:29:30 +0100

    refactor: more idiomatic code

--- server/src/main/kotlin/com/larsreimann/api_editor/validation/AnnotationValidator.kt
+++ server/src/main/kotlin/com/larsreimann/api_editor/validation/AnnotationValidator.kt
@@ -1,21 +1,17 @@
 package com.larsreimann.api_editor.validation
 
+import com.larsreimann.api_editor.model.AnnotationTarget
+import com.larsreimann.api_editor.model.EditorAnnotation
+import com.larsreimann.api_editor.model.GroupAnnotation
 import com.larsreimann.api_editor.model.SerializablePythonClass
 import com.larsreimann.api_editor.model.SerializablePythonFunction
 import com.larsreimann.api_editor.model.SerializablePythonModule
 import com.larsreimann.api_editor.model.SerializablePythonPackage
 import com.larsreimann.api_editor.model.SerializablePythonParameter
-import com.larsreimann.api_editor.model.AnnotationTarget
-import com.larsreimann.api_editor.model.EditorAnnotation
-import com.larsreimann.api_editor.model.GroupAnnotation
 
 class AnnotationValidator(private val annotatedPythonPackage: SerializablePythonPackage) {
-    private val validationErrors: MutableList<AnnotationError>
+    private val validationErrors = mutableListOf<AnnotationError>()
     private val groupAnnotationName = "Group"
-
-    init {
-        validationErrors = ArrayList()
-    }
 
     /**
      * Validates the classes annotated python package and returns validation errors.
@@ -23,97 +19,70 @@ class AnnotationValidator(private val annotatedPythonPackage: SerializablePython
      * @return the validation errors found
      */
     fun validate(): List<AnnotationError> {
+        validationErrors.clear()
         validatePackage()
         return validationErrors
     }
 
     private fun validatePackage() {
-        annotatedPythonPackage.modules.forEach { annotatedPythonModule: SerializablePythonModule ->
-            validateModule(
-                annotatedPythonModule
-            )
-        }
+        annotatedPythonPackage.modules.forEach { validateModule(it) }
     }
 
     private fun validateModule(annotatedPythonModule: SerializablePythonModule) {
-        annotatedPythonModule.classes.forEach { annotatedPythonClass: SerializablePythonClass ->
-            validateClass(
-                annotatedPythonClass
-            )
-        }
-        annotatedPythonModule.functions.forEach { annotatedPythonFunction: SerializablePythonFunction ->
-            validateGlobalFunction(
-                annotatedPythonFunction
-            )
-        }
+        annotatedPythonModule.classes.forEach { validateClass(it) }
+        annotatedPythonModule.functions.forEach { validateGlobalFunction(it) }
     }
 
     private fun validateClass(annotatedPythonClass: SerializablePythonClass) {
-        annotatedPythonClass.methods.forEach { annotatedPythonFunction: SerializablePythonFunction ->
-            validateMethod(
-                annotatedPythonFunction
-            )
-        }
-        val classAnnotations: List<EditorAnnotation> = annotatedPythonClass.annotations
+        annotatedPythonClass.methods.forEach { validateMethod(it) }
+
         validateAnnotationsValidOnTarget(
-            classAnnotations,
-            AnnotationTarget.CLASS, annotatedPythonClass.qualifiedName
+            annotatedPythonClass.annotations,
+            AnnotationTarget.CLASS,
+            annotatedPythonClass.qualifiedName
         )
         validateAnnotationCombinations(
-            classAnnotations,
+            annotatedPythonClass.annotations,
             annotatedPythonClass.qualifiedName
         )
     }
 
     private fun validateMethod(annotatedPythonFunction: SerializablePythonFunction) {
-        val groupedParameterNames = getParameterNamesInGroups(
-            annotatedPythonFunction.annotations
-        )
+        val groupedParameterNames = getParameterNamesInGroups(annotatedPythonFunction.annotations)
         if (annotatedPythonFunction.isConstructor()) {
-            annotatedPythonFunction.parameters.forEach { parameter: SerializablePythonParameter ->
-                validateConstructorParameter(
-                    parameter,
-                    groupedParameterNames.contains(parameter.name)
-                )
+            annotatedPythonFunction.parameters.forEach {
+                validateConstructorParameter(it, groupedParameterNames.contains(it.name))
             }
         } else {
-            annotatedPythonFunction.parameters.forEach { parameter: SerializablePythonParameter ->
-                validateFunctionParameter(
-                    parameter,
-                    groupedParameterNames.contains(parameter.name)
-                )
+            annotatedPythonFunction.parameters.forEach {
+                validateFunctionParameter(it, groupedParameterNames.contains(it.name))
             }
         }
-        val functionAnnotations: List<EditorAnnotation> = annotatedPythonFunction.annotations
+
         validateAnnotationsValidOnTarget(
-            functionAnnotations,
+            annotatedPythonFunction.annotations,
             AnnotationTarget.METHOD,
             annotatedPythonFunction.qualifiedName
         )
         validateAnnotationCombinations(
-            functionAnnotations,
+            annotatedPythonFunction.annotations,
             annotatedPythonFunction.qualifiedName
         )
     }
 
-    private fun validateGlobalFunction(
-        annotatedPythonFunction: SerializablePythonFunction
-    ) {
+    private fun validateGlobalFunction(annotatedPythonFunction: SerializablePythonFunction) {
         val groupedParameterNames = getParameterNamesInGroups(annotatedPythonFunction.annotations)
         annotatedPythonFunction.parameters.forEach {
-            validateFunctionParameter(
-                it,
-                groupedParameterNames.contains(it.name)
-            )
+            validateFunctionParameter(it, groupedParameterNames.contains(it.name))
         }
-        val functionAnnotations: List<EditorAnnotation> = annotatedPythonFunction.annotations
+
         validateAnnotationsValidOnTarget(
-            functionAnnotations,
+            annotatedPythonFunction.annotations,
             AnnotationTarget.GLOBAL_FUNCTION,
             annotatedPythonFunction.qualifiedName
         )
         validateAnnotationCombinations(
-            functionAnnotations,
+            annotatedPythonFunction.annotations,
             annotatedPythonFunction.qualifiedName
         )
     }
@@ -122,19 +91,21 @@ class AnnotationValidator(private val annotatedPythonPackage: SerializablePython
         annotatedPythonParameter: SerializablePythonParameter,
         parameterInGroup: Boolean
     ) {
-        val parameterAnnotations: List<EditorAnnotation> = annotatedPythonParameter.annotations
-        val qualifiedName = annotatedPythonParameter.qualifiedName
         validateAnnotationsValidOnTarget(
-            parameterAnnotations,
+            annotatedPythonParameter.annotations,
             AnnotationTarget.FUNCTION_PARAMETER,
-            qualifiedName
+            annotatedPythonParameter.qualifiedName
         )
         validateAnnotationCombinations(
-            parameterAnnotations,
-            qualifiedName
+            annotatedPythonParameter.annotations,
+            annotatedPythonParameter.qualifiedName
         )
+
         if (parameterInGroup) {
-            validateGroupCombinations(qualifiedName, parameterAnnotations)
+            validateGroupCombinations(
+                annotatedPythonParameter.qualifiedName,
+                annotatedPythonParameter.annotations
+            )
         }
     }
 
@@ -142,19 +113,21 @@ class AnnotationValidator(private val annotatedPythonPackage: SerializablePython
         annotatedPythonParameter: SerializablePythonParameter,
         parameterInGroup: Boolean
     ) {
-        val parameterAnnotations: List<EditorAnnotation> = annotatedPythonParameter.annotations
-        val qualifiedName = annotatedPythonParameter.qualifiedName
         validateAnnotationsValidOnTarget(
-            parameterAnnotations,
+            annotatedPythonParameter.annotations,
             AnnotationTarget.CONSTRUCTOR_PARAMETER,
-            qualifiedName
+            annotatedPythonParameter.qualifiedName
         )
         validateAnnotationCombinations(
-            parameterAnnotations,
-            qualifiedName
+            annotatedPythonParameter.annotations,
+            annotatedPythonParameter.qualifiedName
         )
+
         if (parameterInGroup) {
-            validateGroupCombinations(qualifiedName, parameterAnnotations)
+            validateGroupCombinations(
+                annotatedPythonParameter.qualifiedName,
+                annotatedPythonParameter.annotations
+            )
         }
     }
 
@@ -165,58 +138,22 @@ class AnnotationValidator(private val annotatedPythonPackage: SerializablePython
     ) {
         for (editorAnnotation in editorAnnotations) {
             if (!editorAnnotation.isApplicableTo(target)) {
-                validationErrors.add(
-                    AnnotationTargetError(
-                        qualifiedName,
-                        editorAnnotation.type,
-                        target
-                    )
-                )
+                validationErrors.add(AnnotationTargetError(qualifiedName, editorAnnotation.type, target))
             }
         }
     }
 
-    private fun getParameterNamesInGroups(
-        editorAnnotations: List<EditorAnnotation>
-    ): Set<String> {
-        val groupAnnotations = getGroupAnnotations(
-            editorAnnotations
-        )
-        if (groupAnnotations.isEmpty()) {
-            return emptySet()
-        }
-        val groupedParameterNames: MutableSet<String> = HashSet()
-        groupAnnotations.forEach { (_, parameters): GroupAnnotation ->
-            groupedParameterNames.addAll(
-                parameters
-            )
-        }
-        return groupedParameterNames
+    private fun getParameterNamesInGroups(editorAnnotations: List<EditorAnnotation>): Set<String> {
+        return editorAnnotations
+            .filterIsInstance<GroupAnnotation>()
+            .flatMap { it.parameters }
+            .toSet()
     }
 
-    private fun getGroupAnnotations(
-        editorAnnotations: List<EditorAnnotation>
-    ): List<GroupAnnotation> {
-        val groupAnnotations: MutableList<GroupAnnotation> = ArrayList()
-        editorAnnotations.forEach { annotation: EditorAnnotation ->
-            if (annotation.type == "Group") {
-                groupAnnotations.add(annotation as GroupAnnotation)
-            }
-        }
-        return groupAnnotations
-    }
-
-    private fun validateAnnotationCombinations(
-        editorAnnotations: List<EditorAnnotation>,
-        qualifiedName: String
-    ) {
+    private fun validateAnnotationCombinations(editorAnnotations: List<EditorAnnotation>, qualifiedName: String) {
         for (i in editorAnnotations.indices) {
             for (j in i + 1 until editorAnnotations.size) {
-                validateAnnotationCombination(
-                    qualifiedName,
-                    editorAnnotations[i],
-                    editorAnnotations[j]
-                )
+                validateAnnotationCombination(qualifiedName, editorAnnotations[i], editorAnnotations[j])
             }
         }
     }
@@ -228,76 +165,42 @@ class AnnotationValidator(private val annotatedPythonPackage: SerializablePython
     ) {
         val firstAnnotationName = firstAnnotation.type
         val secondAnnotationName = secondAnnotation.type
-        if (possibleCombinations[firstAnnotationName]!!
-            .isEmpty() ||
-            !possibleCombinations[firstAnnotationName]!!.contains(secondAnnotationName)
-        ) {
-            validationErrors.add(
-                AnnotationCombinationError(
-                    qualifiedName,
-                    firstAnnotationName,
-                    secondAnnotationName
-                )
-            )
+        if (secondAnnotationName !in possibleCombinations[firstAnnotationName]!!) {
+            validationErrors.add(AnnotationCombinationError(qualifiedName, firstAnnotationName, secondAnnotationName))
         }
     }
 
-    private fun validateGroupCombinations(
-        qualifiedName: String,
-        editorAnnotations: List<EditorAnnotation>
-    ) {
-        editorAnnotations.forEach { editorAnnotation: EditorAnnotation ->
-            val annotationName = editorAnnotation.type
-            if (!possibleCombinations[groupAnnotationName]!!.contains(annotationName)
-            ) {
-                validationErrors.add(
-                    GroupAnnotationCombinationError(
-                        qualifiedName,
-                        annotationName
-                    )
-                )
+    private fun validateGroupCombinations(qualifiedName: String, editorAnnotations: List<EditorAnnotation>) {
+        editorAnnotations.forEach {
+            val annotationName = it.type
+            if (annotationName !in possibleCombinations[groupAnnotationName]!!) {
+                validationErrors.add(GroupAnnotationCombinationError(qualifiedName, annotationName))
             }
         }
     }
 
     companion object {
-        private var possibleCombinations: MutableMap<String, Set<String>> = HashMap()
-
-        init {
-            possibleCombinations["Attribute"] = mutableSetOf(
-                "Boundary", "Enum", "Rename"
+        private var possibleCombinations = buildMap<String, Set<String>> {
+            this["Attribute"] = mutableSetOf("Boundary", "Enum", "Rename")
+            this["Boundary"] = mutableSetOf("Attribute", "Group", "Optional", "Rename", "Required")
+            this["CalledAfter"] = mutableSetOf("CalledAfter", "Group", "Move", "Rename")
+            this["Constant"] = mutableSetOf()
+            this["Enum"] = mutableSetOf("Attribute", "Group", "Optional", "Rename", "Required")
+            this["Group"] = mutableSetOf("CalledAfter", "Group", "Move", "Rename")
+            this["Move"] = mutableSetOf("CalledAfter", "Group", "Rename")
+            this["Optional"] = mutableSetOf("Boundary", "Enum", "Group", "Rename")
+            this["Rename"] = mutableSetOf(
+                "Attribute",
+                "Boundary",
+                "CalledAfter",
+                "Enum",
+                "Group",
+                "Move",
+                "Optional",
+                "Required"
             )
-            possibleCombinations["Boundary"] =
-                mutableSetOf(
-                    "Attribute", "Group", "Optional", "Rename", "Required"
-                )
-            possibleCombinations["CalledAfter"] = mutableSetOf(
-                "CalledAfter", "Group", "Move", "Rename"
-            )
-            possibleCombinations["Constant"] = mutableSetOf()
-            possibleCombinations["Enum"] =
-                mutableSetOf(
-                    "Attribute", "Group", "Optional", "Rename", "Required"
-                )
-            possibleCombinations["Group"] = mutableSetOf(
-                "CalledAfter", "Group", "Move", "Rename"
-            )
-            possibleCombinations["Move"] =
-                mutableSetOf(
-                    "CalledAfter", "Group", "Rename"
-                )
-            possibleCombinations["Optional"] = mutableSetOf(
-                "Boundary", "Enum", "Group", "Rename"
-            )
-            possibleCombinations["Rename"] =
-                mutableSetOf(
-                    "Attribute", "Boundary", "CalledAfter", "Enum", "Group", "Move", "Optional", "Required"
-                )
-            possibleCombinations["Required"] =
-                mutableSetOf(
-                    "Boundary", "Enum", "Group", "Rename"
-                )
-            possibleCombinations["Unused"] = mutableSetOf()
+            this["Required"] = mutableSetOf("Boundary", "Enum", "Group", "Rename")
+            this["Unused"] = mutableSetOf()
         }
     }
 }
