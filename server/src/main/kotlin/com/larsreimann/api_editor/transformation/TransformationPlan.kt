a99a2b70036ebe8cf1b22f4dd85d74b2eb96ec6d
Lars Reimann <mail@larsreimann.com>
2022-01-04 10:00:46 +0100

    refactor: clean transformation plan

--- server/src/main/kotlin/com/larsreimann/api_editor/transformation/TransformationPlan.kt
+++ server/src/main/kotlin/com/larsreimann/api_editor/transformation/TransformationPlan.kt
@@ -1,26 +1,39 @@
 package com.larsreimann.api_editor.transformation
 
 import com.larsreimann.api_editor.model.SerializablePythonPackage
+import com.larsreimann.api_editor.mutable_model.MutablePythonPackage
 import com.larsreimann.api_editor.mutable_model.convertPackage
 
 fun processPackage(originalPythonPackage: SerializablePythonPackage): SerializablePythonPackage {
     var modifiedPythonPackage = originalPythonPackage
 
-    // Create original declarations
-    modifiedPythonPackage = modifiedPythonPackage.accept(Preprocessor())!!
+    // Preprocess
+    modifiedPythonPackage = modifiedPythonPackage.preprocess()
 
-    // Apply annotations (don't change the order)
+    // Process annotations
     val mutablePackage = convertPackage(modifiedPythonPackage)
-    mutablePackage.processUnusedAnnotations()
-    mutablePackage.processRenameAnnotations()
-    mutablePackage.processMoveAnnotations()
-    mutablePackage.processParameterAnnotations()
-    mutablePackage.processBoundaryAnnotations()
-    mutablePackage.processPureAnnotations()
+    mutablePackage.processAnnotations()
     modifiedPythonPackage = convertPackage(mutablePackage)
 
-    // Cleanup
-    modifiedPythonPackage = modifiedPythonPackage.accept(Postprocessor)!!
+    // Postprocess
+    modifiedPythonPackage = modifiedPythonPackage.postprocess()
 
     return modifiedPythonPackage
+}
+
+private fun SerializablePythonPackage.preprocess(): SerializablePythonPackage {
+    return this.accept(Preprocessor())!!
+}
+
+private fun MutablePythonPackage.processAnnotations() {
+    this.processUnusedAnnotations()
+    this.processRenameAnnotations()
+    this.processMoveAnnotations()
+    this.processParameterAnnotations()
+    this.processBoundaryAnnotations()
+    this.processPureAnnotations()
+}
+
+private fun SerializablePythonPackage.postprocess(): SerializablePythonPackage {
+    return this.accept(Postprocessor)!!
 }
