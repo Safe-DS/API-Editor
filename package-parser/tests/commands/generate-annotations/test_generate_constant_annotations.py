fefda4bba2700e4c01537cf6cc36f16b52ede9e4
Arsam Islami <arsamislami@yahoo.de>
2022-04-08 17:16:39 +0200

    feat: Started issue #434 - not working yet!

---
+++ package-parser/tests/commands/generate-annotations/test_generate_constant_annotations.py
@@ -0,0 +1,9 @@
+import pytest
+
+def test_irgendwas():
+
+
+    suggest_improvements(
+        "C:\Users\Arsam\Desktop\Arsam\AWT\\api-editor\\api-editor\data\\api\scikit-learn__sklearn__1.0.2__api.json",
+        args.usages
+    )
