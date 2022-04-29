c6a429018a374b7386c4cfc16c39add04ddf266b
Lars Reimann <mail@larsreimann.com>
2022-04-29 19:11:05 +0200

    build: bump Gradle dependencies

--- api-editor/server/build.gradle.kts
+++ api-editor/server/build.gradle.kts
@@ -31,7 +31,7 @@ tasks.withType<KotlinCompile> {
 val ktorVersion = "1.6.7"
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:1.2.10")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
     implementation("io.ktor:ktor-serialization:$ktorVersion")
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-host-common:$ktorVersion")
@@ -40,12 +40,12 @@ dependencies {
 
     // We can later pull this from Maven Central (or some other repo) once published
     implementation(files("lib/de.unibonn.simpleml-1.0.0-SNAPSHOT.jar"))
-    implementation("org.eclipse.xtext:org.eclipse.xtext:2.26.0.M3")
+    implementation("org.eclipse.xtext:org.eclipse.xtext:2.26.0")
 
     testImplementation(kotlin("test"))
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.1.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.2.3")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
-    testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("io.mockk:mockk:1.12.3")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
 }
 
