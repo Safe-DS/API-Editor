c31737e2cec4a78b78bf84f240ff5def2139de46
IAmAlife <iapplefaker@gmail.com>
2022-04-29 11:36:50 +0200

    fixed imports again

--- package-parser/tests/commands/generate_annotations/test_generate_annotations.py
+++ package-parser/tests/commands/generate_annotations/test_generate_annotations.py
@@ -3,11 +3,11 @@ import os
 import pytest
 from pathlib import Path
 
-from commands.find_usages import UsageStore
-from commands.get_api import API
+from package_parser.commands.find_usages import UsageStore
 from package_parser.commands.generate_annotations.generate_annotations import (
     generate_annotations, __get_unused_annotations, __get_constant_annotations, __qname_to_target_name,
     _preprocess_usages)
+from package_parser.commands.get_api import API
 
 UNUSED_EXPECTED = {"unused": {'test/test/Unused_Class': {'target': 'test/test/Unused_Class'},
                               'test/test/commonly_used_global_function/unused_optional_parameter': {
