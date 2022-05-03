9d3aa744032b38a75069701dc0d1aa876e9f0681
Lars Reimann <mail@larsreimann.com>
2022-05-03 10:51:31 +0200

    refactor: use TypeScript parameter properties

--- api-editor/client/src/features/annotatedPackageData/model/AnnotatedPythonModule.ts
+++ api-editor/client/src/features/annotatedPackageData/model/AnnotatedPythonModule.ts
@@ -5,26 +5,12 @@ import AnnotatedPythonClass from './AnnotatedPythonClass';
 import AnnotatedPythonFunction from './AnnotatedPythonFunction';
 
 export default class AnnotatedPythonModule {
-    readonly name: string;
-    readonly imports: PythonImport[];
-    readonly fromImports: PythonFromImport[];
-    readonly classes: AnnotatedPythonClass[];
-    readonly functions: AnnotatedPythonFunction[];
-    readonly annotations: InferableAnnotation[];
-
     constructor(
-        name: string,
-        imports: PythonImport[] = [],
-        fromImports: PythonFromImport[] = [],
-        classes: AnnotatedPythonClass[] = [],
-        functions: AnnotatedPythonFunction[] = [],
-        annotations: InferableAnnotation[] = [],
-    ) {
-        this.name = name;
-        this.imports = imports;
-        this.fromImports = fromImports;
-        this.classes = classes;
-        this.functions = functions;
-        this.annotations = annotations;
-    }
+        readonly name: string,
+        readonly imports: PythonImport[] = [],
+        readonly fromImports: PythonFromImport[] = [],
+        readonly classes: AnnotatedPythonClass[] = [],
+        readonly functions: AnnotatedPythonFunction[] = [],
+        readonly annotations: InferableAnnotation[] = [],
+    ) {}
 }
