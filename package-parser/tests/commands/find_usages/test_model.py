61a922377a097aa7e0963245326c78fa65355f5c
Masara <Masara@users.noreply.github.com>
2022-05-06 12:47:40 +0000

    style: apply automatic fixes of linters

--- package-parser/tests/commands/find_usages/test_model.py
+++ package-parser/tests/commands/find_usages/test_model.py
@@ -5,7 +5,9 @@ from package_parser.commands.find_usages import UsageStore
 
 
 def setup():
-    usages_json_path = os.path.join(os.getcwd(), "tests", "commands", "find_usages", "data", "usage_data.json")
+    usages_json_path = os.path.join(
+        os.getcwd(), "tests", "commands", "find_usages", "data", "usage_data.json"
+    )
 
     with open(usages_json_path, "r") as usages_file:
         usages_json = json.load(usages_file)
@@ -20,21 +22,19 @@ def test_to_count_json():
     result = usages.to_count_json()
 
     assert result == {
-        'class_counts': {
-            'test.Commonly_Used_Class': 2,
-            'test.Unused_Class': 1
+        "class_counts": {"test.Commonly_Used_Class": 2, "test.Unused_Class": 1},
+        "function_counts": {
+            "test.commonly_used_function": 2,
+            "test.unused_function": 1,
         },
-        'function_counts': {
-            'test.commonly_used_function': 2,
-            'test.unused_function': 1
+        "parameter_counts": {
+            "test.commonly_used_global_function.often_used_parameter": 2,
+            "test.uncommonly_used_global_function.never_used_parameter": 1,
         },
-        'parameter_counts': {
-            'test.commonly_used_global_function.often_used_parameter': 2,
-            'test.uncommonly_used_global_function.never_used_parameter': 1
-        },
-        'value_counts': {
-            'test.commonly_used_global_function.often_used_parameter': {
-                "'bla'": 2, "'blup'": 1
+        "value_counts": {
+            "test.commonly_used_global_function.often_used_parameter": {
+                "'bla'": 2,
+                "'blup'": 1,
             }
-        }
+        },
     }
