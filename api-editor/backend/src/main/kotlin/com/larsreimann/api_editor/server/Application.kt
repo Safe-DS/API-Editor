717189dc493472b5441459b7c0e3618127d53ad4
Lars Reimann <mail@larsreimann.com>
2022-05-12 09:57:46 +0200

    refactor: rename components of editor

---
+++ api-editor/backend/src/main/kotlin/com/larsreimann/api_editor/server/Application.kt
@@ -0,0 +1,43 @@
+package com.larsreimann.api_editor.backend
+
+import de.unibonn.simpleml.SimpleMLStandaloneSetup
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.plugins.callloging.CallLogging
+import io.ktor.server.plugins.compression.Compression
+import io.ktor.server.plugins.compression.deflate
+import io.ktor.server.plugins.compression.gzip
+import io.ktor.server.plugins.compression.minimumSize
+import io.ktor.server.request.path
+import org.slf4j.event.Level
+
+fun main() {
+    SimpleMLStandaloneSetup.doSetup()
+
+    embeddedServer(Netty, port = 4280, host = "localhost") {
+        configureHTTP()
+        configureMonitoring()
+        configureRouting()
+    }.start(wait = true)
+}
+
+fun Application.configureHTTP() {
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+}
+
+fun Application.configureMonitoring() {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+}
