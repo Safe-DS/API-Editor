81ff5add4323a024321d6a91216d266daa8478b8
lars-reimann <lars-reimann@users.noreply.github.com>
2022-05-20 12:58:31 +0000

    style: apply automatic fixes of linters

--- data/client/titanic.py
+++ data/client/titanic.py
@@ -19,31 +19,29 @@ numerical_cols = ["Age", "SibSp", "Parch"]
 categorical_cols = ["Sex", "Pclass"]
 
 # Preprocessing for numerical data
-numerical_transformer = SimpleImputer(strategy='constant')
+numerical_transformer = SimpleImputer(strategy="constant")
 
 # Preprocessing for categorical data
-ohe = OneHotEncoder(handle_unknown='ignore')
-categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('onehot', ohe)
-])
+ohe = OneHotEncoder(handle_unknown="ignore")
+categorical_transformer = Pipeline(
+    steps=[("imputer", SimpleImputer(strategy="most_frequent")), ("onehot", ohe)]
+)
 
 # Bundle preprocessing for numerical and categorical data
 preprocessor = ColumnTransformer(
     transformers=[
-        ('num', numerical_transformer, numerical_cols),
-        ('cat', categorical_transformer, categorical_cols)
-    ], remainder="drop")
+        ("num", numerical_transformer, numerical_cols),
+        ("cat", categorical_transformer, categorical_cols),
+    ],
+    remainder="drop",
+)
 
 model = RandomForestClassifier(n_estimators=500)
 # model = GridSearchCV(estimator, [{
 #     "learning_rate": [0.1]
 # }], scoring="accuracy")
 
-pipeline = Pipeline(steps=[
-    ("preprocessor", preprocessor),
-    ("model", model)
-])
+pipeline = Pipeline(steps=[("preprocessor", preprocessor), ("model", model)])
 
 pipeline.fit(X, y)
 
@@ -51,7 +49,5 @@ test = pd.read_csv("data/test.csv", index_col="PassengerId")
 test = test.drop(["Name", "Ticket", "Cabin"], axis=1)
 predictions = pipeline.predict(test).round().astype(int)
 
-output = pd.DataFrame({
-    "Survived": predictions
-}, index=test.index)
+output = pd.DataFrame({"Survived": predictions}, index=test.index)
 output.to_csv("data/predictions.csv")
