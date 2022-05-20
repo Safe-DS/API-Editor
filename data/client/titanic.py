63105ebf3e083bd4c714176fa093aba9578f75d3
jofaul <54673768+jofaul@users.noreply.github.com>
2022-05-20 15:03:36 +0200

    feat: Combine console commands into "all" command (#491)
    * All command added
    
    * style: apply automatic fixes of linters
    
    * ci: update smoke tests
    
    * fix: Fixed Path Variable type
    
    * Renamed smoke test
    
    * Fixed parallel running of api and usages for all command, changed generate_annotations parameters to Path instead of TextIOWrapper
    
    * Docs and renaming
    
    * style: apply automatic fixes of linters
    
    * Fixed bug where private parameters etc are checked as well, even when they shouldnt be.
    
    * style: apply automatic fixes of linters
    
    * Changed annotations args to Path
    
    * Fixed name of smoke test
    
    * docs: update README
    
    * refactor: rename src to client
    
    * ci: add smoke tests for the remaining commands
    
    * style: apply automatic fixes of linters
    
    Co-authored-by: jofaul <jofaul@users.noreply.github.com>
    Co-authored-by: Lars Reimann <mail@larsreimann.com>
    Co-authored-by: GideonKoenig <ginosiassa.koenig@online.de>
    Co-authored-by: GideonKoenig <44088734+GideonKoenig@users.noreply.github.com>
    Co-authored-by: lars-reimann <lars-reimann@users.noreply.github.com>

---
+++ data/client/titanic.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+
+data = pd.read_csv("data/train.csv", index_col="PassengerId")
+data = data.drop(["Name", "Ticket", "Cabin"], axis=1)
+data["Young"] = data["Age"] < 18
+
+X = data.drop("Survived", axis=1)
+y = data.Survived
+
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+
+numerical_cols = ["Age", "SibSp", "Parch"]
+categorical_cols = ["Sex", "Pclass"]
+
+# Preprocessing for numerical data
+numerical_transformer = SimpleImputer(strategy="constant")
+
+# Preprocessing for categorical data
+ohe = OneHotEncoder(handle_unknown="ignore")
+categorical_transformer = Pipeline(
+    steps=[("imputer", SimpleImputer(strategy="most_frequent")), ("onehot", ohe)]
+)
+
+# Bundle preprocessing for numerical and categorical data
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("num", numerical_transformer, numerical_cols),
+        ("cat", categorical_transformer, categorical_cols),
+    ],
+    remainder="drop",
+)
+
+model = RandomForestClassifier(n_estimators=500)
+# model = GridSearchCV(estimator, [{
+#     "learning_rate": [0.1]
+# }], scoring="accuracy")
+
+pipeline = Pipeline(steps=[("preprocessor", preprocessor), ("model", model)])
+
+pipeline.fit(X, y)
+
+test = pd.read_csv("data/test.csv", index_col="PassengerId")
+test = test.drop(["Name", "Ticket", "Cabin"], axis=1)
+predictions = pipeline.predict(test).round().astype(int)
+
+output = pd.DataFrame({"Survived": predictions}, index=test.index)
+output.to_csv("data/predictions.csv")
