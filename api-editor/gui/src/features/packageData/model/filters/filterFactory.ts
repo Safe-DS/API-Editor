0f6d871b6fb5f57f85427bcdcc307b60a0f0e3ec
Lars Reimann <mail@larsreimann.com>
2022-06-05 20:18:50 +0200

    feat: filters `is:required` and `is:optional`

--- api-editor/gui/src/features/packageData/model/filters/filterFactory.ts
+++ api-editor/gui/src/features/packageData/model/filters/filterFactory.ts
@@ -1,16 +1,17 @@
-import { ConjunctiveFilter } from './ConjunctiveFilter';
+import {ConjunctiveFilter} from './ConjunctiveFilter';
 import NameFilter from './NameFilter';
 import AbstractPythonFilter from './AbstractPythonFilter';
-import DeclarationTypeFilter, { DeclarationType } from './DeclarationTypeFilter';
-import VisibilityFilter, { Visibility } from './VisibilityFilter';
-import { NegatedFilter } from './NegatedFilter';
-import { Optional } from '../../../../common/util/types';
-import AnnotationFilter, { AnnotationType } from './AnnotationFilter';
+import DeclarationTypeFilter, {DeclarationType} from './DeclarationTypeFilter';
+import VisibilityFilter, {Visibility} from './VisibilityFilter';
+import {NegatedFilter} from './NegatedFilter';
+import {Optional} from '../../../../common/util/types';
+import AnnotationFilter, {AnnotationType} from './AnnotationFilter';
 import UsageFilter from './UsageFilter';
 import UsefulnessFilter from './UsefulnessFilter';
-import { equals, greaterThan, greaterThanOrEqual, lessThan, lessThanOrEqual } from './comparisons';
+import {equals, greaterThan, greaterThanOrEqual, lessThan, lessThanOrEqual} from './comparisons';
 import ParameterAssignmentFilter from './ParameterAssignmentFilter';
-import { PythonParameterAssignment } from '../PythonParameter';
+import {PythonParameterAssignment} from '../PythonParameter';
+import {RequiredOrOptional, RequiredOrOptionalFilter} from "./RequiredOrOptionalFilter";
 
 /**
  * Creates a filter from the given string. This method handles conjunctions, negations, and non-negated tokens.
@@ -70,6 +71,12 @@ const parsePositiveToken = function (token: string): Optional<AbstractPythonFilt
             return new VisibilityFilter(Visibility.Public);
         case 'is:internal':
             return new VisibilityFilter(Visibility.Internal);
+
+        // Parameter required or optional
+        case 'is:required':
+            return new RequiredOrOptionalFilter(RequiredOrOptional.Required);
+        case 'is:optional':
+            return new RequiredOrOptionalFilter(RequiredOrOptional.Optional);
 
         // Parameter assignment
         case 'is:implicit':
