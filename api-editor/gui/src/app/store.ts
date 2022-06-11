494f8e658277b2e39a63bf03e1bc142a7c5c546b
Lars Reimann <mail@larsreimann.com>
2022-06-11 12:48:05 +0200

    fix: disable serialization warnings

--- api-editor/gui/src/app/store.ts
+++ api-editor/gui/src/app/store.ts
@@ -1,6 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
-import { annotationsReducer } from '../features/annotations/annotationSlice';
+import {configureStore} from '@reduxjs/toolkit';
+import {setupListeners} from '@reduxjs/toolkit/query';
+import {annotationsReducer} from '../features/annotations/annotationSlice';
 import {apiReducer} from '../features/packageData/apiSlice';
 import {usageReducer} from '../features/usages/usageSlice';
 import {uiReducer} from "../features/ui/uiSlice";
@@ -12,6 +12,19 @@ export const store = configureStore({
         ui: uiReducer,
         usages: usageReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                // Ignore these action types
+                ignoredActions: [
+                    'usages/initialize',
+                    'usages/initialize/fulfilled',
+                    'usages/set'
+                ],
+                // Ignore these paths in the state
+                ignoredPaths: ['usages.usages'],
+            },
+        }),
 });
 
 setupListeners(store.dispatch);
