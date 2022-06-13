cca834d14320b2b01485656f7aaa9959ff25aa1c
Lars Reimann <mail@larsreimann.com>
2022-06-13 15:38:44 +0200

    fix(package-parser): visibility of function/parameters in re-exported classes (#630)
    * fix: don't remove internal usages, so we don't add remove annotations for them
    
    * fix: add 0 usages for all classes/functions/parameters
    
    * fix(package-parser): properly compute visibility of function/parameters in re-exported classes
    
    * chore: update API data
    
    * chore: update annotation data
    
    * style: apply automatic fixes of linters
    
    Co-authored-by: lars-reimann <lars-reimann@users.noreply.github.com>

--- package-parser/package_parser/processing/annotations/_usages_preprocessor.py
+++ package-parser/package_parser/processing/annotations/_usages_preprocessor.py
@@ -1,48 +1,11 @@
-import logging
-
 from package_parser.model.api import API
 from package_parser.model.usages import UsageCountStore
 from package_parser.utils import parent_id
 
 
 def _preprocess_usages(usages: UsageCountStore, api: API) -> None:
-    _remove_internal_usages(usages, api)
     _add_unused_api_elements(usages, api)
     _add_implicit_usages_of_default_value(usages, api)
-
-
-def _remove_internal_usages(usages: UsageCountStore, api: API) -> None:
-    """
-    Removes usages of internal parts of the API. It might incorrectly remove some calls to methods that are inherited
-    from internal classes into a public class but these are just fit/predict/etc., i.e. something we want to keep
-    unchanged anyway.
-
-    :param usages: Usage store
-    :param api: Description of the API
-    """
-
-    # Internal classes
-    for class_id in list(usages.class_usages.keys()):
-        if not api.is_public_class(class_id):
-            logging.info(f"Removing usages of internal class {class_id}")
-            usages.remove_class(class_id)
-
-    # Internal functions
-    for function_id in list(usages.function_usages.keys()):
-        if not api.is_public_function(function_id):
-            logging.info(f"Removing usages of internal function {function_id}")
-            usages.remove_function(function_id)
-
-    # Internal parameters
-    parameter_ids_in_api = set(api.parameters().keys())
-
-    for parameter_id in list(usages.parameter_usages.keys()):
-        function_id = parent_id(parameter_id)
-        if parameter_id not in parameter_ids_in_api or not api.is_public_function(
-            function_id
-        ):
-            logging.info(f"Removing usages of internal parameter {parameter_id}")
-            usages.remove_parameter(parameter_id)
 
 
 def _add_unused_api_elements(usages: UsageCountStore, api: API) -> None:
@@ -54,20 +17,15 @@ def _add_unused_api_elements(usages: UsageCountStore, api: API) -> None:
     :param api: Description of the API
     """
 
-    # Public classes
     for class_id in api.classes:
-        if api.is_public_class(class_id):
-            usages.add_class_usages(class_id, 0)
+        usages.add_class_usages(class_id, 0)
 
-    # Public functions
     for function in api.functions.values():
-        if api.is_public_function(function.id):
-            usages.add_function_usages(function.id, 0)
+        usages.add_function_usages(function.id, 0)
 
-            # "Public" parameters
-            for parameter in function.parameters:
-                usages.init_value(parameter.id)
-                usages.add_parameter_usages(parameter.id, 0)
+        for parameter in function.parameters:
+            usages.add_parameter_usages(parameter.id, 0)
+            usages.init_value(parameter.id)
 
 
 def _add_implicit_usages_of_default_value(usages: UsageCountStore, api: API) -> None:
