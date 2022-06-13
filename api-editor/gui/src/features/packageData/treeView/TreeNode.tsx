e2b277adac54e17564bb98321c28de598c1226df
Lars Reimann <mail@larsreimann.com>
2022-06-13 11:41:11 +0200

    feat(gui): expand only when icon is clicked or node is clicked with Shift button active

--- api-editor/gui/src/features/packageData/treeView/TreeNode.tsx
+++ api-editor/gui/src/features/packageData/treeView/TreeNode.tsx
@@ -1,21 +1,21 @@
-import { HStack, Icon, Text as ChakraText } from '@chakra-ui/react';
-import React, { MouseEvent } from 'react';
-import { IconType } from 'react-icons/lib';
-import { useLocation } from 'react-router';
-import { useNavigate } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../../../app/hooks';
-import { PythonDeclaration } from '../model/PythonDeclaration';
+import {HStack, Icon, Text as ChakraText} from '@chakra-ui/react';
+import React, {MouseEvent} from 'react';
+import {IconType} from 'react-icons/lib';
+import {useLocation} from 'react-router';
+import {useNavigate} from 'react-router-dom';
+import {useAppDispatch, useAppSelector} from '../../../app/hooks';
+import {PythonDeclaration} from '../model/PythonDeclaration';
 import {
     HeatMapMode,
     selectHeatMapMode,
     selectIsExpandedInTreeView,
     toggleIsExpandedInTreeView,
 } from '../../ui/uiSlice';
-import { VisibilityIndicator } from './VisibilityIndicator';
-import { AbstractPythonFilter } from '../model/filters/AbstractPythonFilter';
-import { selectAnnotations } from '../../annotations/annotationSlice';
-import { HeatMapInterpolation, HeatMapTag } from './HeatMapTag';
-import { UsageCountStore } from '../../usages/model/UsageCountStore';
+import {VisibilityIndicator} from './VisibilityIndicator';
+import {AbstractPythonFilter} from '../model/filters/AbstractPythonFilter';
+import {selectAnnotations} from '../../annotations/annotationSlice';
+import {HeatMapInterpolation, HeatMapTag} from './HeatMapTag';
+import {UsageCountStore} from '../../usages/model/UsageCountStore';
 
 interface TreeNodeProps {
     declaration: PythonDeclaration;
@@ -38,14 +38,14 @@ export class ValuePair {
 }
 
 export const TreeNode: React.FC<TreeNodeProps> = function ({
-    declaration,
-    icon,
-    isExpandable,
-    filter,
-    usages,
-    maxValue,
-    specificValue = 0,
-}) {
+                                                               declaration,
+                                                               icon,
+                                                               isExpandable,
+                                                               filter,
+                                                               usages,
+                                                               maxValue,
+                                                               specificValue = 0,
+                                                           }) {
     const currentPathname = useLocation().pathname;
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
@@ -60,13 +60,18 @@ export const TreeNode: React.FC<TreeNodeProps> = function ({
 
     const fontWeight = filter.shouldKeepDeclaration(declaration, annotations, usages) ? 'bold' : undefined;
 
-    const handleClick = (event: MouseEvent) => {
-        if (!event.shiftKey) {
+    const handleNodeClick = (event: MouseEvent) => {
+        if (event.shiftKey) {
             dispatch(toggleIsExpandedInTreeView(declaration.pathAsString()));
+        } else {
+            navigate(`/${declaration.pathAsString()}`);
         }
+    }
 
-        navigate(`/${declaration.pathAsString()}`);
-    };
+    const handleVisibilityIndicatorClick = (event: MouseEvent) => {
+        dispatch(toggleIsExpandedInTreeView(declaration.pathAsString()));
+        event.stopPropagation();
+    }
 
     const interpolation =
         useAppSelector(selectHeatMapMode) === HeatMapMode.Annotations
@@ -81,16 +86,17 @@ export const TreeNode: React.FC<TreeNodeProps> = function ({
             color={color}
             backgroundColor={backgroundColor}
             paddingLeft={paddingLeft}
-            onClick={handleClick}
+            onClick={handleNodeClick}
         >
             <VisibilityIndicator
                 hasChildren={isExpandable}
                 showChildren={showChildren}
                 isSelected={isSelected(declaration, currentPathname)}
+                onClick={handleVisibilityIndicatorClick}
             />
-            <Icon as={icon} />
+            <Icon as={icon}/>
             {displayHeatMap && (
-                <HeatMapTag actualValue={specificValue} maxValue={maxValue} interpolation={interpolation} />
+                <HeatMapTag actualValue={specificValue} maxValue={maxValue} interpolation={interpolation}/>
             )}
             <ChakraText fontWeight={fontWeight}>{declaration.getUniqueName()}</ChakraText>
         </HStack>
