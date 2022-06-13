0037adac7b1a3e5ad2b407598af3a5705f6f76c1
lars-reimann <lars-reimann@users.noreply.github.com>
2022-06-13 09:48:02 +0000

    style: apply automatic fixes of linters

--- api-editor/gui/src/features/packageData/treeView/TreeNode.tsx
+++ api-editor/gui/src/features/packageData/treeView/TreeNode.tsx
@@ -1,21 +1,21 @@
-import {HStack, Icon, Text as ChakraText} from '@chakra-ui/react';
-import React, {MouseEvent} from 'react';
-import {IconType} from 'react-icons/lib';
-import {useLocation} from 'react-router';
-import {useNavigate} from 'react-router-dom';
-import {useAppDispatch, useAppSelector} from '../../../app/hooks';
-import {PythonDeclaration} from '../model/PythonDeclaration';
+import { HStack, Icon, Text as ChakraText } from '@chakra-ui/react';
+import React, { MouseEvent } from 'react';
+import { IconType } from 'react-icons/lib';
+import { useLocation } from 'react-router';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { PythonDeclaration } from '../model/PythonDeclaration';
 import {
     HeatMapMode,
     selectHeatMapMode,
     selectIsExpandedInTreeView,
     toggleIsExpandedInTreeView,
 } from '../../ui/uiSlice';
-import {VisibilityIndicator} from './VisibilityIndicator';
-import {AbstractPythonFilter} from '../model/filters/AbstractPythonFilter';
-import {selectAnnotations} from '../../annotations/annotationSlice';
-import {HeatMapInterpolation, HeatMapTag} from './HeatMapTag';
-import {UsageCountStore} from '../../usages/model/UsageCountStore';
+import { VisibilityIndicator } from './VisibilityIndicator';
+import { AbstractPythonFilter } from '../model/filters/AbstractPythonFilter';
+import { selectAnnotations } from '../../annotations/annotationSlice';
+import { HeatMapInterpolation, HeatMapTag } from './HeatMapTag';
+import { UsageCountStore } from '../../usages/model/UsageCountStore';
 
 interface TreeNodeProps {
     declaration: PythonDeclaration;
@@ -38,14 +38,14 @@ export class ValuePair {
 }
 
 export const TreeNode: React.FC<TreeNodeProps> = function ({
-                                                               declaration,
-                                                               icon,
-                                                               isExpandable,
-                                                               filter,
-                                                               usages,
-                                                               maxValue,
-                                                               specificValue = 0,
-                                                           }) {
+    declaration,
+    icon,
+    isExpandable,
+    filter,
+    usages,
+    maxValue,
+    specificValue = 0,
+}) {
     const currentPathname = useLocation().pathname;
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
@@ -66,12 +66,12 @@ export const TreeNode: React.FC<TreeNodeProps> = function ({
         } else {
             navigate(`/${declaration.pathAsString()}`);
         }
-    }
+    };
 
     const handleVisibilityIndicatorClick = (event: MouseEvent) => {
         dispatch(toggleIsExpandedInTreeView(declaration.pathAsString()));
         event.stopPropagation();
-    }
+    };
 
     const interpolation =
         useAppSelector(selectHeatMapMode) === HeatMapMode.Annotations
@@ -94,9 +94,9 @@ export const TreeNode: React.FC<TreeNodeProps> = function ({
                 isSelected={isSelected(declaration, currentPathname)}
                 onClick={handleVisibilityIndicatorClick}
             />
-            <Icon as={icon}/>
+            <Icon as={icon} />
             {displayHeatMap && (
-                <HeatMapTag actualValue={specificValue} maxValue={maxValue} interpolation={interpolation}/>
+                <HeatMapTag actualValue={specificValue} maxValue={maxValue} interpolation={interpolation} />
             )}
             <ChakraText fontWeight={fontWeight}>{declaration.getUniqueName()}</ChakraText>
         </HStack>
