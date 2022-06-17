f25062aa4d369fa1601a20a23a3ef3778389b590
jofaul <54673768+jofaul@users.noreply.github.com>
2022-06-17 22:45:37 +0200

    feat: Added value distribution bar chart for parameters in GUI (#668)
    * Parameter view now shows a usage barchart with colors dependent on if a value is a literal or not
    
    * Removed comments
    
    * Added chart.js
    
    * build(gui): update package-lock.json
    
    * style(gui): fix lint errors
    
    * feat(gui): swap type and default value
    
    * fix(gui): also allow decimal numbers
    
    * refactor(gui): move select call to component that uses data
    
    * style: apply automatic fixes of linters
    
    Co-authored-by: Lars Reimann <mail@larsreimann.com>
    Co-authored-by: lars-reimann <lars-reimann@users.noreply.github.com>

--- api-editor/gui/src/features/packageData/selectionView/ParameterView.tsx
+++ api-editor/gui/src/features/packageData/selectionView/ParameterView.tsx
@@ -2,15 +2,42 @@ import { Heading, Stack, Text as ChakraText } from '@chakra-ui/react';
 import React from 'react';
 import { PythonParameter } from '../model/PythonParameter';
 import { ParameterNode } from './ParameterNode';
+import {
+    BarElement,
+    CategoryScale,
+    Chart as ChartJS,
+    LinearScale,
+    LineElement,
+    PointElement,
+    Title,
+    Tooltip,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { useAppSelector } from '../../../app/hooks';
+import { selectUsages } from '../../usages/usageSlice';
+
+ChartJS.register(CategoryScale, PointElement, LineElement, LinearScale, BarElement, Title, Tooltip);
 
 interface ParameterViewProps {
     pythonParameter: PythonParameter;
 }
 
 export const ParameterView: React.FC<ParameterViewProps> = function ({ pythonParameter }) {
+    const usages = useAppSelector(selectUsages);
+    const parameterUsages = usages.valueUsages.get(pythonParameter.id);
+
     return (
         <Stack spacing={8}>
             <ParameterNode isTitle pythonParameter={pythonParameter} />
+
+            {pythonParameter.typeInDocs && (
+                <Stack spacing={4}>
+                    <Heading as="h4" size="md">
+                        Type
+                    </Heading>
+                    <ChakraText paddingLeft={4}>{pythonParameter.typeInDocs}</ChakraText>
+                </Stack>
+            )}
 
             {pythonParameter.defaultValue && (
                 <Stack spacing={4}>
@@ -21,14 +48,63 @@ export const ParameterView: React.FC<ParameterViewProps> = function ({ pythonPar
                 </Stack>
             )}
 
-            {pythonParameter.typeInDocs && (
+            {parameterUsages && (
                 <Stack spacing={4}>
                     <Heading as="h4" size="md">
-                        Type
+                        Most common values
                     </Heading>
-                    <ChakraText paddingLeft={4}>{pythonParameter.typeInDocs}</ChakraText>
+                    {createBarChart(parameterUsages)}
                 </Stack>
             )}
         </Stack>
     );
+};
+
+const createBarChart = function (parameterUsages: Map<string, number>): React.ReactElement {
+    const options = {
+        indexAxis: 'y' as const,
+        elements: {
+            bar: {
+                borderWidth: 2,
+            },
+        },
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false as const,
+            },
+        },
+    };
+
+    const sortedParameterUsages = new Map([...parameterUsages.entries()].sort((a, b) => b[1] - a[1]));
+    const labels = [...sortedParameterUsages.keys()].slice(0, 10);
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                data: labels.map((key) => sortedParameterUsages.get(key)),
+                borderColor: labels.map((key) => (isStringifiedLiteral(key) ? '#871F78' : '#888888')),
+                backgroundColor: labels.map((key) => (isStringifiedLiteral(key) ? '#871F78' : '#888888')),
+            },
+        ],
+    };
+
+    return <Bar options={options} data={data} />;
+};
+
+const isStringifiedLiteral = function (value: string): boolean {
+    if (value === 'None') {
+        return true;
+    }
+    if (value.startsWith('"') && value.endsWith('"')) {
+        return true;
+    }
+    if (value.startsWith("'") && value.endsWith("'")) {
+        return true;
+    }
+    if (value === 'True' || value === 'False') {
+        return true;
+    }
+    return Boolean(value.match(/^[+-]?\d+(\.\d*)?$/u));
 };
