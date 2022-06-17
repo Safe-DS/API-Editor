afd71bd16be5c7fcaae9038fcd8ce1079eb45987
Lars Reimann <mail@larsreimann.com>
2022-06-17 15:46:51 +0200

    fix(gui): don't batch add `@remove` annotations to parameters

--- api-editor/gui/src/features/annotations/batchforms/RemoveBatchForm.tsx
+++ api-editor/gui/src/features/annotations/batchforms/RemoveBatchForm.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { useAppDispatch } from '../../../app/hooks';
-import { PythonDeclaration } from '../../packageData/model/PythonDeclaration';
-import { RemoveAnnotation, upsertRemoves } from '../annotationSlice';
-import { PythonModule } from '../../packageData/model/PythonModule';
-import { EmptyBatchForm } from './EmptyBatchForm';
-import { PythonPackage } from '../../packageData/model/PythonPackage';
+import {useAppDispatch} from '../../../app/hooks';
+import {PythonDeclaration} from '../../packageData/model/PythonDeclaration';
+import {RemoveAnnotation, upsertRemoves} from '../annotationSlice';
+import {EmptyBatchForm} from './EmptyBatchForm';
+import {PythonClass} from "../../packageData/model/PythonClass";
+import {PythonFunction} from "../../packageData/model/PythonFunction";
 
 interface RemoveBatchFormProps {
     targets: PythonDeclaration[];
 }
 
 export const RemoveBatchForm: React.FC<RemoveBatchFormProps> = function ({ targets }) {
-    const filteredTargets = targets.filter((t) => !(t instanceof PythonModule || t instanceof PythonPackage));
+    const filteredTargets = targets.filter((t) => t instanceof PythonClass || t instanceof PythonFunction);
     const targetPaths = filteredTargets.map((t) => t.id);
 
     // Hooks -----------------------------------------------------------------------------------------------------------
