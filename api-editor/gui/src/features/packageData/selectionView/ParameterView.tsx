da33a9868ef750d6429a96e2792ea83068df16e6
jofaul <iapplefaker@gmail.com>
2022-06-17 16:12:37 +0200

    Parameter view now shows a usage barchart with colors dependent on if a value is a literal or not

--- api-editor/gui/src/features/packageData/selectionView/ParameterView.tsx
+++ api-editor/gui/src/features/packageData/selectionView/ParameterView.tsx
@@ -1,16 +1,35 @@
-import { Heading, Stack, Text as ChakraText } from '@chakra-ui/react';
-import React from 'react';
-import { PythonParameter } from '../model/PythonParameter';
-import { ParameterNode } from './ParameterNode';
+import {Heading, Stack, Text as ChakraText} from '@chakra-ui/react';
+import React, {ReactElement} from 'react';
+import {PythonParameter} from '../model/PythonParameter';
+import {ParameterNode} from './ParameterNode';
+import {UsageCountStore} from "../../usages/model/UsageCountStore";
+import {
+    BarElement,
+    CategoryScale,
+    Chart as ChartJS,
+    LinearScale,
+    LineElement,
+    PointElement,
+    Title,
+    Tooltip,
+} from 'chart.js';
+import {Bar} from 'react-chartjs-2';
+
+ChartJS.register(CategoryScale, PointElement, LineElement, LinearScale, BarElement, Title, Tooltip);
+
 
 interface ParameterViewProps {
     pythonParameter: PythonParameter;
+    usages: UsageCountStore;
 }
 
-export const ParameterView: React.FC<ParameterViewProps> = function ({ pythonParameter }) {
+export const ParameterView: React.FC<ParameterViewProps> = function ({pythonParameter, usages}) {
+    const parameterUsages = usages.valueUsages.get(pythonParameter.id);
+    //parameterUsages is of type Map<string, number>
+    //only use the 10 most used parameters
     return (
         <Stack spacing={8}>
-            <ParameterNode isTitle pythonParameter={pythonParameter} />
+            <ParameterNode isTitle pythonParameter={pythonParameter}/>
 
             {pythonParameter.defaultValue && (
                 <Stack spacing={4}>
@@ -29,6 +48,69 @@ export const ParameterView: React.FC<ParameterViewProps> = function ({ pythonPar
                     <ChakraText paddingLeft={4}>{pythonParameter.typeInDocs}</ChakraText>
                 </Stack>
             )}
+
+            {parameterUsages && (
+                <Stack spacing={4}>
+                    <Heading as="h4" size="md">
+                        Usages
+                    </Heading>
+                    {createBarChart(parameterUsages, pythonParameter.name)}
+                </Stack>
+            )}
         </Stack>
     );
 };
+
+let createBarChart = function (parameterUsages: Map<string, number>, parameter: string): ReactElement {
+    const options = {
+        indexAxis: 'y' as const,
+        elements: {
+            bar: {
+                borderWidth: 2,
+            },
+        },
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false as const,
+            },
+            title: {
+                display: true,
+                text: `${parameter} usages`,
+            },
+        },
+    };
+
+    const sortedParameterUsages = new Map([...parameterUsages.entries()].sort((a, b) => b[1] - a[1]));
+    const labels = [...sortedParameterUsages.keys()].slice(0, 10);
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                data: labels.map((key) => sortedParameterUsages.get(key)),
+                borderColor: labels.map((key) => isStringifiedLiteral(key) ? '#871F78' : '#888888'),
+                backgroundColor: labels.map((key) => isStringifiedLiteral(key) ? '#871F78' : '#888888'),
+            },
+        ],
+    };
+
+    return <Bar options={options} data={data}/>;
+};
+
+let isStringifiedLiteral = function (value: string): boolean {
+    if (value === 'None') {
+        return true;
+    }
+    if (value.startsWith('"') && value.endsWith('"')) {
+        return true;
+    }
+    if (value.startsWith("'") && value.endsWith("'")) {
+        return true;
+    }
+    if (value === "True" || value === "False") {
+        return true;
+    }
+    return !!value.match(/^-?\d+$/);
+
+}
--- api-editor/gui/src/features/packageData/selectionView/SelectionView.tsx
+++ api-editor/gui/src/features/packageData/selectionView/SelectionView.tsx
@@ -1,19 +1,21 @@
-import { Box, VStack } from '@chakra-ui/react';
+import {Box, VStack} from '@chakra-ui/react';
 import React from 'react';
-import { useLocation } from 'react-router';
-import { PythonClass } from '../model/PythonClass';
-import { PythonFunction } from '../model/PythonFunction';
-import { PythonModule } from '../model/PythonModule';
-import { PythonParameter } from '../model/PythonParameter';
-import { ClassView } from './ClassView';
-import { FunctionView } from './FunctionView';
-import { ModuleView } from './ModuleView';
-import { ParameterView } from './ParameterView';
-import { useAppSelector } from '../../../app/hooks';
-import { selectRawPythonPackage } from '../apiSlice';
+import {useLocation} from 'react-router';
+import {PythonClass} from '../model/PythonClass';
+import {PythonFunction} from '../model/PythonFunction';
+import {PythonModule} from '../model/PythonModule';
+import {PythonParameter} from '../model/PythonParameter';
+import {ClassView} from './ClassView';
+import {FunctionView} from './FunctionView';
+import {ModuleView} from './ModuleView';
+import {ParameterView} from './ParameterView';
+import {useAppSelector} from '../../../app/hooks';
+import {selectRawPythonPackage} from '../apiSlice';
+import {selectUsages} from "../../usages/usageSlice";
 
 export const SelectionView: React.FC = function () {
     const rawPythonPackage = useAppSelector(selectRawPythonPackage);
+    const usages = useAppSelector(selectUsages);
     const declaration = rawPythonPackage.getDeclarationById(useLocation().pathname.split('/').splice(1).join('/'));
 
     if (!declaration) {
@@ -24,10 +26,11 @@ export const SelectionView: React.FC = function () {
         <VStack h="100%" spacing={0}>
             <Box flexGrow={1} overflowY="auto" width="100%">
                 <Box padding={4}>
-                    {declaration instanceof PythonFunction && <FunctionView pythonFunction={declaration} />}
-                    {declaration instanceof PythonClass && <ClassView pythonClass={declaration} />}
-                    {declaration instanceof PythonModule && <ModuleView pythonModule={declaration} />}
-                    {declaration instanceof PythonParameter && <ParameterView pythonParameter={declaration} />}
+                    {declaration instanceof PythonFunction && <FunctionView pythonFunction={declaration}/>}
+                    {declaration instanceof PythonClass && <ClassView pythonClass={declaration}/>}
+                    {declaration instanceof PythonModule && <ModuleView pythonModule={declaration}/>}
+                    {declaration instanceof PythonParameter &&
+                        <ParameterView pythonParameter={declaration} usages={usages}/>}
                 </Box>
             </Box>
         </VStack>
