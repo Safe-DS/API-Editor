49a6546949bb673114a7b6b352a564c90ce3564f
GideonKoenig <GideonKoenig@users.noreply.github.com>
2022-06-17 13:47:51 +0000

    style: apply automatic fixes of linters

--- api-editor/gui/src/features/packageData/selectionView/StatisticsView.tsx
+++ api-editor/gui/src/features/packageData/selectionView/StatisticsView.tsx
@@ -11,7 +11,7 @@ import {
 } from 'chart.js';
 import { Bar, Line } from 'react-chartjs-2';
 import { PythonPackage } from '../model/PythonPackage';
-import {Center, Spacer, Wrap, WrapItem} from '@chakra-ui/react';
+import { Center, Spacer, Wrap, WrapItem } from '@chakra-ui/react';
 import { UsageCountStore } from '../../usages/model/UsageCountStore';
 
 ChartJS.register(CategoryScale, PointElement, LineElement, LinearScale, BarElement, Title, Tooltip);
@@ -61,44 +61,32 @@ export const StatisticsView: React.FC<FunctionViewProps> = function ({ pythonPac
     );
 
     return (
-        <Wrap w='100%'>
-            <WrapItem flex='0 0 100%'>
-                <WrapItem w='33%'>
-                    <Center w='100%'>
-                        {classBarChart}
-                    </Center>
+        <Wrap w="100%">
+            <WrapItem flex="0 0 100%">
+                <WrapItem w="33%">
+                    <Center w="100%">{classBarChart}</Center>
                 </WrapItem>
-                <Spacer/>
-                <WrapItem w='33%'>
-                    <Center w='100%'>
-                        {functionBarChart}
-                    </Center>
+                <Spacer />
+                <WrapItem w="33%">
+                    <Center w="100%">{functionBarChart}</Center>
                 </WrapItem>
-                <Spacer/>
-                <WrapItem w='33%'>
-                    <Center w='100%'>
-                        {parameterBarChart}
-                    </Center>
+                <Spacer />
+                <WrapItem w="33%">
+                    <Center w="100%">{parameterBarChart}</Center>
                 </WrapItem>
             </WrapItem>
 
-            <WrapItem flex='0 0 100%'>
-                <WrapItem w='33%'>
-                    <Center w='100%'>
-                        {classLineChart}
-                    </Center>
+            <WrapItem flex="0 0 100%">
+                <WrapItem w="33%">
+                    <Center w="100%">{classLineChart}</Center>
                 </WrapItem>
-                <Spacer/>
-                <WrapItem w='33%'>
-                    <Center w='100%'>
-                        {functionLineChart}
-                    </Center>
+                <Spacer />
+                <WrapItem w="33%">
+                    <Center w="100%">{functionLineChart}</Center>
                 </WrapItem>
-                <Spacer/>
-                <WrapItem w='33%'>
-                    <Center w='100%'>
-                        {parameterLineChart}
-                    </Center>
+                <Spacer />
+                <WrapItem w="33%">
+                    <Center w="100%">{parameterLineChart}</Center>
                 </WrapItem>
             </WrapItem>
         </Wrap>
