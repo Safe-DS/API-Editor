3cd7bff799fea35c8179c46692d125b36e6f4ca8
Lars Reimann <mail@larsreimann.com>
2022-06-23 15:43:12 +0200

    build(deps): bump plugin.serialization from 1.6.21 to 1.7.0 in /api-editor

--- api-editor/build.gradle.kts
+++ api-editor/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
     // Pin versions for subprojects
     kotlin("jvm") version "1.7.0" apply false
-    kotlin("plugin.serialization") version "1.6.21" apply false
+    kotlin("plugin.serialization") version "1.7.0" apply false
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
     id("com.github.node-gradle.node") version "3.3.0" apply false
     id("org.jetbrains.compose") version "1.2.0-alpha01-dev724" apply false
