b19fea176ecf6152967e2bd745c85327c6c4772c
Lars Reimann <mail@larsreimann.com>
2022-06-23 14:16:57 +0200

    build(desktop): Kotlin 1.7

--- api-editor/build.gradle.kts
+++ api-editor/build.gradle.kts
@@ -5,16 +5,17 @@ plugins {
     idea
 
     // Pin versions for subprojects
-    kotlin("jvm") version "1.6.21" apply false
-    kotlin("plugin.serialization") version "1.6.21" apply false
+    kotlin("jvm") version "1.7.0" apply false
+    kotlin("plugin.serialization") version "1.7.0" apply false
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
     id("com.github.node-gradle.node") version "3.3.0" apply false
-    id("org.jetbrains.compose") version "1.2.0-alpha01-dev686" apply false
+    id("org.jetbrains.compose") version "1.2.0-alpha01-dev724" apply false
 }
 
 repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("https://androidx.dev/storage/compose-compiler/repository/")
     google()
 }
 
@@ -38,6 +39,17 @@ subprojects {
     repositories {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://androidx.dev/storage/compose-compiler/repository/")
         google()
+    }
+}
+
+allprojects {
+    configurations.all {
+        resolutionStrategy.dependencySubstitution {
+            substitute(module("org.jetbrains.compose.compiler:compiler")).apply {
+                using(module("androidx.compose.compiler:compiler:1.2.0-dev-k1.7.0-53370d83bb1"))
+            }
+        }
     }
 }
