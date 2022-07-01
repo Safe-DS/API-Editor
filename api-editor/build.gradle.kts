41152e2f6f0cfd678713d08b75133ab66eafe71e
Lars Reimann <mail@larsreimann.com>
2022-07-01 23:47:13 +0200

    build(deps): remove beta repositories

--- api-editor/build.gradle.kts
+++ api-editor/build.gradle.kts
@@ -16,8 +16,6 @@ repositories {
     mavenCentral()
     // Can be removed once compose-jb supports Kotlin 1.7.0
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    // Can be removed once compose-jb supports Kotlin 1.7.0
-    maven("https://androidx.dev/storage/compose-compiler/repository/")
     google()
 }
 
@@ -42,8 +40,6 @@ subprojects {
         mavenCentral()
         // Can be removed once compose-jb supports Kotlin 1.7.0
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        // Can be removed once compose-jb supports Kotlin 1.7.0
-        maven("https://androidx.dev/storage/compose-compiler/repository/")
         google()
     }
 }
