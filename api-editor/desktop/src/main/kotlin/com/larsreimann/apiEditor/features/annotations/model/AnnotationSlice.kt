61b825e52c2c6ecc90418412390dd5989339c418
Lars Reimann <mail@larsreimann.com>
2022-07-05 18:13:14 +0200

    chore(desktop): continue work on model

--- api-editor/desktop/src/main/kotlin/com/larsreimann/apiEditor/features/annotations/model/AnnotationSlice.kt
+++ api-editor/desktop/src/main/kotlin/com/larsreimann/apiEditor/features/annotations/model/AnnotationSlice.kt
@@ -5,7 +5,9 @@ import androidx.compose.runtime.remember
 import com.larsreimann.apiEditor.features.api.ApiElementId
 
 // Since we need to mutate this data, there is no additional AnnotationStore class
+// TODO: Maybe it's still worth to have an annotation store? this way we minimize the logic that is tied
+//       to compose, particularly the generation of annotations
 
 class AnnotationSlice {
-    val targetToAnnotations = remember { mutableStateMapOf<ApiElementId, List<Annotation>>() }
+    private val targetToAnnotations = remember { mutableStateMapOf<ApiElementId, List<Annotation>>() }
 }
