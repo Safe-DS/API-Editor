9bcc9b9ba56f9f2025a23d038b8498ad3c686ce6
Lars Reimann <mail@larsreimann.com>
2022-07-07 11:23:46 +0200

    build(desktop): configure kotest

--- api-editor/desktop/build.gradle.kts
+++ api-editor/desktop/build.gradle.kts
@@ -45,8 +45,10 @@ dependencies {
 
     testImplementation(kotlin("test"))
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.3.2")
+    testImplementation("io.kotest:kotest-framework-datatest:5.3.2")
+    testImplementation("io.kotest:kotest-property:5.3.2")
+    testImplementation("io.kotest:kotest-runner-junit5:5.3.2")
     testImplementation("io.mockk:mockk:1.12.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
 }
 
 // Tasks ---------------------------------------------------------------------------------------------------------------
