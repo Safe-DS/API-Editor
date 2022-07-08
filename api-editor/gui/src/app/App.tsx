0f039884bc881fd7e268d2b2ba7cea662c28d287
lars-reimann <lars-reimann@users.noreply.github.com>
2022-07-08 17:36:49 +0000

    style: apply automatic fixes of linters

--- api-editor/gui/src/app/App.tsx
+++ api-editor/gui/src/app/App.tsx
@@ -194,13 +194,7 @@ export const App: React.FC = function () {
                     </Box>
                 </GridItem>
                 {showStatistics && (
-                    <GridItem
-                        gridArea="rightPane"
-                        overflow="auto"
-                        w="20vw"
-                        borderLeft={1}
-                        layerStyle="subtleBorder"
-                    >
+                    <GridItem gridArea="rightPane" overflow="auto" w="20vw" borderLeft={1} layerStyle="subtleBorder">
                         <Box padding={4}>
                             <StatisticsView />
                         </Box>
