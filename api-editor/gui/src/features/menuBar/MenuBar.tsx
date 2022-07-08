779e69328e8c87242b3d7448c6a79286ee2ec968
lars-reimann <lars-reimann@users.noreply.github.com>
2022-07-08 08:07:43 +0000

    style: apply automatic fixes of linters

--- api-editor/gui/src/features/menuBar/MenuBar.tsx
+++ api-editor/gui/src/features/menuBar/MenuBar.tsx
@@ -64,7 +64,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { SelectionBreadcrumbs } from './SelectionBreadcrumbs';
 import { HelpMenu } from './HelpMenu';
 import { AnnotationStore } from '../annotations/versioning/AnnotationStoreV2';
-import {supportedAnnotationStoreSchemaVersions} from "../annotations/versioning/expectedVersions";
+import { supportedAnnotationStoreSchemaVersions } from '../annotations/versioning/expectedVersions';
 
 interface MenuBarProps {
     displayInferErrors: (errors: string[]) => void;
@@ -94,12 +94,22 @@ export const MenuBar: React.FC<MenuBarProps> = function ({ displayInferErrors })
 
     const exportAnnotations = () => {
         const a = document.createElement('a');
-        const file = new Blob([JSON.stringify({
-            ...annotationStore,
-            schemaVersion: supportedAnnotationStoreSchemaVersions[supportedAnnotationStoreSchemaVersions.length - 1],
-        }, null, 4)], {
-            type: 'application/json',
-        });
+        const file = new Blob(
+            [
+                JSON.stringify(
+                    {
+                        ...annotationStore,
+                        schemaVersion:
+                            supportedAnnotationStoreSchemaVersions[supportedAnnotationStoreSchemaVersions.length - 1],
+                    },
+                    null,
+                    4,
+                ),
+            ],
+            {
+                type: 'application/json',
+            },
+        );
         a.href = URL.createObjectURL(file);
         a.download = 'annotations.json';
         a.click();
