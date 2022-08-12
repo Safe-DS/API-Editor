ffe4fdbc34b4a7cf5b9f4baf715dc71e85cddbdb
Lars Reimann <mail@larsreimann.com>
2022-08-12 13:33:57 +0200

    feat(gui): optional/require movement statistics

--- api-editor/gui/src/features/statistics/PaperStatistics.tsx
+++ api-editor/gui/src/features/statistics/PaperStatistics.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import { PythonPackage } from '../packageData/model/PythonPackage';
 import { UsageCountStore } from '../usages/model/UsageCountStore';
-import { AnnotationStore } from '../annotations/versioning/AnnotationStoreV2';
+import {AnnotationStore, DefaultValue, DefaultValueType} from '../annotations/versioning/AnnotationStoreV2';
 import { useAppSelector } from '../../app/hooks';
 import { selectRawPythonPackage } from '../packageData/apiSlice';
 import { selectUsages } from '../usages/usageSlice';
 import { selectAnnotationStore } from '../annotations/annotationSlice';
 import { createFilterFromString } from '../filter/model/filterFactory';
 import { AbstractPythonFilter } from '../filter/model/AbstractPythonFilter';
+import {PythonParameterAssignment} from "../packageData/model/PythonParameter";
 
 export const PaperStatistics = function () {
     const pythonPackage = useAppSelector(selectRawPythonPackage);
@@ -16,32 +17,43 @@ export const PaperStatistics = function () {
 
     useEffect(() => {
         apiSize(pythonPackage, usages, annotations);
-    }, [pythonPackage, usages, annotations])
+        optionalAndRequired(pythonPackage, usages, annotations);
+    }, [pythonPackage, usages, annotations]);
 
     return <>Test</>;
 };
 
 const apiSize = function (pythonPackage: PythonPackage, usages: UsageCountStore, annotations: AnnotationStore) {
-    const counter = countMatches(pythonPackage, usages, annotations)
+    const counter = countMatches(pythonPackage, usages, annotations);
     const classesPublicFilter = createFilterFromString('is:class is:public');
     const functionsPublicFilter = createFilterFromString('is:function is:public');
     const parameterPublicFilter = createFilterFromString('is:parameter is:public !is:implicit');
 
-    const byThreshold: string[] = []
+    const byThreshold: string[] = [];
 
-    for (let threshold = 0; threshold <= 99; threshold++) {
-        const classesUsedFilter = createFilterFromString(`is:class is:public usages:>${threshold}`)
-        const functionsUsedFilter = createFilterFromString(`is:function is:public usages:>${threshold}`)
-        const parametersUsedFilter = createFilterFromString(`is:parameter is:public !is:implicit usages:>${threshold}`)
-        const parametersUsefulFilter = createFilterFromString(`is:parameter is:public !is:implicit usefulness:>${threshold}`)
+    for (let threshold = 1; threshold <= 100; threshold++) {
+        const classesUsedFilter = createFilterFromString(`is:class is:public usages:>=${threshold}`);
+        const functionsUsedFilter = createFilterFromString(`is:function is:public usages:>=${threshold}`);
+        const parametersUsedFilter = createFilterFromString(
+            `is:parameter is:public !is:implicit usages:>=${threshold}`,
+        );
+        const parametersUsefulFilter = createFilterFromString(
+            `is:parameter is:public !is:implicit usefulness:>=${threshold}`,
+        );
 
-        byThreshold.push(`  ${threshold} -> <|classesUsed->${counter(classesUsedFilter)}, functionsUsed->${counter(functionsUsedFilter)}, parametersUsed->${counter(parametersUsedFilter)}, parametersUseful->${counter(parametersUsefulFilter)}|>`)
+        byThreshold.push(
+            `  ${threshold} -> <|classesUsed->${counter(classesUsedFilter)}, functionsUsed->${counter(
+                functionsUsedFilter,
+            )}, parametersUsed->${counter(parametersUsedFilter)}, parametersUseful->${counter(
+                parametersUsefulFilter,
+            )}|>`,
+        );
     }
 
     console.log(`classesPublic = ${counter(classesPublicFilter)};
 functionsPublic = ${counter(functionsPublicFilter)};
 parameterPublic = ${counter(parameterPublicFilter)};
-byThreshold = <|\n${byThreshold.join(",\n")}\n|>;
+byThreshold = <|\n${byThreshold.join(',\n')}\n|>;
     `);
 };
 
@@ -59,3 +71,95 @@ const countMatches =
         }
         return result;
     };
+
+const optionalAndRequired = function (
+    pythonPackage: PythonPackage,
+    usages: UsageCountStore,
+    annotations: AnnotationStore,
+) {
+    const publicParameters = pythonPackage
+        .getParameters()
+        .filter((parameter) => parameter.assignedBy !== PythonParameterAssignment.IMPLICIT && parameter.isPublic);
+    const requiredParameters = publicParameters.filter((parameter) => parameter.defaultValue === null);
+    const optionalParameters = publicParameters.filter((parameter) => parameter.defaultValue !== null);
+
+    const requiredToRequired = requiredParameters.filter((parameter) => {
+        const annotation = annotations.valueAnnotations[parameter.id];
+        if (!annotation) {
+            return true;
+        }
+
+        return annotation.variant === 'required';
+    }).length;
+    const requiredToOptional = requiredParameters.filter((parameter) => {
+        const annotation = annotations.valueAnnotations[parameter.id];
+        return annotation?.variant === 'optional';
+    }).length;
+    const requiredToConstant = requiredParameters.filter((parameter) => {
+        const annotation = annotations.valueAnnotations[parameter.id];
+        return annotation?.variant === 'constant';
+    }).length;
+    const optionalToRequired = optionalParameters.filter((parameter) => {
+        const annotation = annotations.valueAnnotations[parameter.id];
+        return annotation?.variant === 'required';
+    }).length;
+    const optionalToOptionalWithSameDefault = optionalParameters.filter((parameter) => {
+        const annotation = annotations.valueAnnotations[parameter.id];
+        if (!annotation) {
+            return true;
+        }
+
+        return annotation?.variant === 'optional' && parameter.defaultValue === stringifiedValue(annotation.defaultValueType, annotation.defaultValue);
+    }).length;
+    const optionalToOptionalWithDifferentDefault = optionalParameters.filter((parameter) => {
+        const annotation = annotations.valueAnnotations[parameter.id];
+        return annotation?.variant === 'optional' && parameter.defaultValue !== stringifiedValue(annotation.defaultValueType, annotation.defaultValue);
+    }).length;
+    const optionalToConstantWithSameDefault = optionalParameters.filter((parameter) => {
+        const annotation = annotations.valueAnnotations[parameter.id];
+        if (!annotation) {
+            return false
+        }
+
+        if (annotation.variant === 'omitted') {
+            return true;
+        }
+
+        return annotation.variant === "constant" && parameter.defaultValue === stringifiedValue(annotation.defaultValueType, annotation.defaultValue);
+    }).length;
+    const optionalToConstantWithDifferentDefault = optionalParameters.filter((parameter) => {
+        const annotation = annotations.valueAnnotations[parameter.id];
+        return annotation?.variant === 'constant' && parameter.defaultValue !== stringifiedValue(annotation.defaultValueType, annotation.defaultValue);
+    }).length;
+
+    console.log(`
+originalPublic: ${publicParameters.length};
+originalRequired: ${requiredParameters.length};
+originalOptional: ${optionalParameters.length};
+
+requiredToRequired: ${requiredToRequired}
+requiredToOptional: ${requiredToOptional}
+requiredToConstant: ${requiredToConstant}
+
+optionalToRequired: ${optionalToRequired}
+optionalToOptionalWithSameDefault: ${optionalToOptionalWithSameDefault}
+optionalToOptionalWithDifferentDefault: ${optionalToOptionalWithDifferentDefault}
+optionalToConstantWithSameDefault: ${optionalToConstantWithSameDefault}
+optionalToConstantWithDifferentDefault: ${optionalToConstantWithDifferentDefault}
+`);
+};
+
+const stringifiedValue = function (defaultValueType: DefaultValueType | undefined, defaultValue: DefaultValue | undefined): string {
+    switch (defaultValueType) {
+        case 'string':
+            return `'${defaultValue}'`;
+        case 'number':
+            return String(defaultValue);
+        case 'boolean':
+            return defaultValue ? 'True' : 'False';
+        case 'none':
+            return 'None';
+        default:
+            return '???';
+    }
+}
