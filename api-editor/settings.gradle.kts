10eb0453c05a0a7ac7910445795b05e357b2d225
Lars Reimann <mail@larsreimann.com>
2022-11-02 10:54:36 +0100

    build(deps-dev): update compose plugin

--- api-editor/settings.gradle.kts
+++ api-editor/settings.gradle.kts
@@ -1,13 +1,5 @@
 rootProject.name = "com.larsreimann.python_api_editor"
 
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        // Can be removed once compose-jb is out of alpha
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
-}
-
 include(
     "backend",
     "desktop",
