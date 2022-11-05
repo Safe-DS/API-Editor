18c2345a4222a361054da9e2aa52cb18ac63b21c
Aclrian <32142988+Aclrian@users.noreply.github.com>
2022-11-05 14:03:15 +0100

    create migrate command

---
+++ package-parser/package_parser/cli/_run_migrate.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+
+from package_parser.processing.api.model import API
+
+
+def _run_migrate_command(
+    apiv1_file_path: Path, annotations_file_path: Path, apiv2_file_path: Path, out_dir_path: Path
+) -> None:
+    apiv1 = _read_api_file(apiv1_file_path)
+    apiv2 = _read_api_file(apiv2_file_path)
+
+
+def _read_api_file(api_file_path: Path) -> API:
+    with open(api_file_path) as api_file:
+        api_json = json.load(api_file)
+
+    return API.from_json(api_json)
