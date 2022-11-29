e64500ce1ff1c27db0facd160d49002f184b031a
Aclrian <32142988+Aclrian@users.noreply.github.com>
2022-11-29 16:09:19 +0100

    remove duplicated file _differ

--- package-parser/package_parser/processing/migration/__init__.py
+++ package-parser/package_parser/processing/migration/__init__.py
@@ -1,10 +1,11 @@
-from ._differ import AbstractDiffer, SimpleDiffer
 from ._migrate import migrate_annotations
 from package_parser.processing.migration.model import (
+    AbstractDiffer,
     APIMapping,
     ManyToManyMapping,
     ManyToOneMapping,
     Mapping,
     OneToManyMapping,
     OneToOneMapping,
+    SimpleDiffer,
 )
