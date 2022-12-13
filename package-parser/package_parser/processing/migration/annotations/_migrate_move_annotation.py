329c657f189807772056dbf555e6e9abb055ec84
Aclrian <Aclrian@users.noreply.github.com>
2022-12-13 17:09:49 +0000

    style: apply automated linter fixes

--- package-parser/package_parser/processing/migration/annotations/_migrate_move_annotation.py
+++ package-parser/package_parser/processing/migration/annotations/_migrate_move_annotation.py
@@ -3,9 +3,16 @@ from copy import deepcopy
 from package_parser.processing.annotations.model import (
     AbstractAnnotation,
     EnumReviewResult,
-    MoveAnnotation, TodoAnnotation,
+    MoveAnnotation,
+    TodoAnnotation,
 )
-from package_parser.processing.api.model import Attribute, Result, Class, Function, Parameter
+from package_parser.processing.api.model import (
+    Attribute,
+    Class,
+    Function,
+    Parameter,
+    Result,
+)
 from package_parser.processing.migration.model import (
     ManyToOneMapping,
     Mapping,
@@ -16,7 +23,7 @@ from ._constants import migration_author
 from ._get_migration_text import get_migration_text
 
 
-def is_moveable(element:  Attribute | Class | Function | Parameter | Result) -> bool:
+def is_moveable(element: Attribute | Class | Function | Parameter | Result) -> bool:
     if isinstance(element, (Attribute, Result)):
         return False
     if isinstance(element, Function):
@@ -42,14 +49,16 @@ def migrate_move_annotation(
         element = mapping.get_apiv2_elements()[0]
         if isinstance(element, (Attribute, Result)) or not is_moveable(element):
             if isinstance(element, Parameter):
-                return [TodoAnnotation(
-                    element.id,
-                    authors,
-                    move_annotation.reviewers,
-                    move_annotation.comment,
-                    EnumReviewResult.NONE,
-                    migrate_text,
-                )]
+                return [
+                    TodoAnnotation(
+                        element.id,
+                        authors,
+                        move_annotation.reviewers,
+                        move_annotation.comment,
+                        EnumReviewResult.NONE,
+                        migrate_text,
+                    )
+                ]
             return []
         move_annotation.target = element.id
         return [move_annotation]
@@ -64,7 +73,11 @@ def migrate_move_annotation(
 
     move_annotations: list[AbstractAnnotation] = []
     for element in mapping.get_apiv2_elements():
-        if isinstance(element, type(annotated_apiv1_element)) and is_moveable(element) and not isinstance(element, (Attribute, Result)):
+        if (
+            isinstance(element, type(annotated_apiv1_element))
+            and is_moveable(element)
+            and not isinstance(element, (Attribute, Result))
+        ):
             move_annotations.append(
                 MoveAnnotation(
                     element.id,
