ab94562e78fe8af228478bfd6e122d568d18bafc
Aclrian <32142988+Aclrian@users.noreply.github.com>
2023-01-03 15:43:38 +0100

    feat: Improve calculating of Levenshtein Distance (#1180)
    Closes ##1179.
    
    ### Summary of Changes
    
    add dependecy levenshtein and use its distance function in
    name_similarity
    
    ### Testing Instructions
    
    run `package-parser/tests/processing/migration/test_differ.py` in a
    previous commit from the main branch and after this commit and compare the running time

--- package-parser/package_parser/processing/migration/model/_differ.py
+++ package-parser/package_parser/processing/migration/model/_differ.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Any, Optional
+from typing import Any, Callable, Optional
 
+from Levenshtein import distance
 from package_parser.processing.api.model import (
     AbstractType,
     Attribute,
@@ -44,7 +45,9 @@ class AbstractDiffer(ABC):
 
 
 def distance_elements(
-    list_a: list[Any], list_b: list[Any], are_similar=lambda x, y: x == y
+    list_a: list[Any],
+    list_b: list[Any],
+    are_similar: Callable[[Any, Any], bool] = lambda x, y: x == y,
 ) -> float:
     if len(list_a) == 0:
         return len(list_b)
@@ -64,7 +67,7 @@ class SimpleDiffer(AbstractDiffer):
         ParameterAssignment, dict[ParameterAssignment, float]
     ]
 
-    def __init__(self):
+    def __init__(self) -> None:
         distance_between_implicit_and_explicit = 0.3
         distance_between_vararg_and_normal = 0.3
         distance_between_position_and_named = 0.3
@@ -173,9 +176,7 @@ class SimpleDiffer(AbstractDiffer):
         return (name_similarity + attributes_similarity + code_similarity) / 3
 
     def _compute_name_similarity(self, name_a: str, name_b: str) -> float:
-        name_similarity = distance_elements([*name_a], [*name_b]) / max(
-            len(name_a), len(name_b), 1
-        )
+        name_similarity = distance(name_a, name_b) / max(len(name_a), len(name_b), 1)
         return 1 - name_similarity
 
     def compute_attribute_similarity(
@@ -208,7 +209,9 @@ class SimpleDiffer(AbstractDiffer):
             function_a.name, function_b.name
         )
 
-        def are_parameters_similar(parameter_a: Parameter, parameter_b: Parameter):
+        def are_parameters_similar(
+            parameter_a: Parameter, parameter_b: Parameter
+        ) -> bool:
             return self.compute_parameter_similarity(parameter_a, parameter_b) == 1
 
         parameter_similarity = distance_elements(
@@ -258,7 +261,7 @@ class SimpleDiffer(AbstractDiffer):
 
         def are_types_similar(
             abstract_type_a: AbstractType, abstract_type_b: AbstractType
-        ):
+        ) -> bool:
             return abstract_type_a.to_json() == abstract_type_b.to_json()
 
         type_list_a = self._create_list_from_type(type_a)
@@ -268,7 +271,7 @@ class SimpleDiffer(AbstractDiffer):
         ) / max(len(type_list_a), len(type_list_b), 1)
         return 1 - diff_elements
 
-    def _create_list_from_type(self, abstract_type: AbstractType):
+    def _create_list_from_type(self, abstract_type: AbstractType) -> list[AbstractType]:
         if isinstance(abstract_type, UnionType):
             return abstract_type.types
         return [abstract_type]
