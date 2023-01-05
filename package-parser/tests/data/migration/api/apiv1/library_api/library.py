6ef5f100cd1302c5824a446d40aea365e389df50
Aclrian <32142988+Aclrian@users.noreply.github.com>
2023-01-05 18:30:22 +0100

    fix linter error

--- package-parser/tests/data/migration/api/apiv1/library_api/library.py
+++ package-parser/tests/data/migration/api/apiv1/library_api/library.py
@@ -1,8 +1,8 @@
 import dataclasses
 from datetime import datetime, timedelta
 
-from .books.book import Book
-from .persons.persons import LibraryUser, Employee
+from .book import Book
+from .persons import LibraryUser, Employee
 
 
 @dataclasses.dataclass
@@ -23,12 +23,14 @@ class Library:
     def return_book(self, book: Book, user: LibraryUser) -> None:
         if book.borrow_by is not None and book.borrow_until is not None and book.borrow_by == user:  # apiv2: check if book is in borrowed book list
             late_fee = 0.0
-            today = datetime.today()
+            today = datetime.today().date()
             if book.borrow_until > today or (book.borrow_by == today and not self.is_open):
                 late_fee = (today - book.borrow_until).days * book.FEE_PER_DAY
                 if not self.is_open:
                     late_fee += 1.0
-            user.give_back(book, late_fee)
+            user.give_back(late_fee)  # apiv2: rename function
+            book.borrow_by = None
+            book.borrow_until = None
             self.borrowed_books.remove(book)
 
     def borrow(self, book: Book, user: LibraryUser) -> None:  # apiv2: check if pending_fees are not above 5
--- package-parser/tests/data/migration/api/apiv1/library_api/persons.py
+++ package-parser/tests/data/migration/api/apiv1/library_api/persons.py
@@ -1,8 +1,6 @@
 import datetime
 from dataclasses import dataclass
 
-from library_api.book import Book
-
 
 @dataclass
 class LibraryUser:
@@ -12,10 +10,8 @@ class LibraryUser:
     pending_fees: float
     member_until: datetime.date
 
-    def give_back(self, book: Book, late_fee: float) -> None:
+    def give_back(self, late_fee: float) -> None:
         self.pending_fees = self.pending_fees + late_fee
-        book.borrow_by = None
-        book.borrow_until = None
 
     def extend_membership(self) -> None:
         self.member_until = self.member_until + datetime.timedelta(days=365)  # apiv2: change to relativedelta(years=1)
