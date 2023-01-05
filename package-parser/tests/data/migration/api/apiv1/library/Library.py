3b9e5e10f8b62405cfe6b2b40891f0ef088ca9f1
Aclrian <32142988+Aclrian@users.noreply.github.com>
2023-01-05 18:08:38 +0100

    add apiv1 part 2/x

--- package-parser/tests/data/migration/api/apiv1/library/Library.py
+++
@@ -1,8 +0,0 @@
-import dataclasses
-
-
-@dataclasses.dataclass
-class Library:
-    book: list[Book]
-    user: list[LibraryUser]
-
---
+++ package-parser/tests/data/migration/api/apiv1/library_api/book.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass, field
+from datetime import date
+from typing import Optional
+
+from library_api.persons import LibraryUser
+
+
+@dataclass
+class Book:
+    FEE_PER_DAY: float = field(init=False)
+    borrow_until: Optional[date]
+    borrow_by: Optional[LibraryUser]
+    genre: str
+    isbn: str
+    author: str
+    release_date: date
+    number_of_times_this_book_was_borrowed: int = 0
+
+    def __post_init__(self) -> None:
+        fee_for_each_genre: dict[str, float] = {
+            "fiction": 1.0,
+            "nonfiction": 2.0,
+            "poetry": 1.5,
+            "drama": 5.0,
+            "high_demand": 10.0,
+            "low_demand": 0.5,
+            "other": 2.5,
+        }
+        if self.genre in fee_for_each_genre:
+            self.FEE_PER_DAY = fee_for_each_genre[self.genre]
+        else:
+            self.FEE_PER_DAY = fee_for_each_genre["other"]
