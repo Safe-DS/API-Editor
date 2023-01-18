0800be200559e24053b9e1770fbb30963b3e2f8c
Aclrian <32142988+Aclrian@users.noreply.github.com>
2023-01-18 20:38:26 +0100

    update api and annotation data

--- package-parser/migration_test_data/api/apiv2/library_apiv2/notificate.py
+++ package-parser/migration_test_data/api/apiv2/library_apiv2/notificate.py
@@ -13,3 +13,14 @@ def notificate(person: Person, message: str) -> None:
     message : str
     """
     print("To: " + person.get_name() + "\n" + person.address + "\n" + message)
+
+
+def send_message_to_person(person: Person, message: str) -> None:
+    """Sends a message to a person
+
+    Parameters
+    ----------
+    person : Person
+    message : str
+    """
+    print("To: " + person.get_name() + "\n" + person.address + "\n" + message)
