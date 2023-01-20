3d90be9bd507f96ab35727061586b11e63957239
Aclrian <Aclrian@users.noreply.github.com>
2023-01-20 16:52:32 +0000

    style: apply automated linter fixes

--- package-parser/package_parser/processing/api/_get_instance_attributes.py
+++ package-parser/package_parser/processing/api/_get_instance_attributes.py
@@ -32,19 +32,33 @@ def get_instance_attributes(class_node: astroid.ClassDef) -> list[Attribute]:
                 )
                 if attribute_type is not None:
                     types.add(attribute_type)
-                elif isinstance(assignment.parent.value, Name) and isinstance(assignment.parent.parent, astroid.FunctionDef) and assignment.parent.parent.name == "__init__":
+                elif (
+                    isinstance(assignment.parent.value, Name)
+                    and isinstance(assignment.parent.parent, astroid.FunctionDef)
+                    and assignment.parent.parent.name == "__init__"
+                ):
                     init_function = assignment.parent.parent
                     parameter_name = assignment.parent.value.name
                     for arg in init_function.args.args:
                         i = init_function.args.args.index(arg)
-                        if isinstance(init_function.args.args[i], (astroid.nodes.node_classes.AssignName, Name)) and init_function.args.args[i].name == parameter_name:
+                        if (
+                            isinstance(
+                                init_function.args.args[i],
+                                (astroid.nodes.node_classes.AssignName, Name),
+                            )
+                            and init_function.args.args[i].name == parameter_name
+                        ):
                             type_hint = init_function.args.annotations[i]
                             if type_hint is not None:
                                 if isinstance(type_hint, Name):
                                     types.add(type_hint.name)
                                 elif isinstance(type_hint, astroid.Attribute):
                                     types.add(type_hint.attrname)
-                                elif isinstance(type_hint, Subscript) and isinstance(type_hint.value, Name) and isinstance(type_hint.slice, Name):
+                                elif (
+                                    isinstance(type_hint, Subscript)
+                                    and isinstance(type_hint.value, Name)
+                                    and isinstance(type_hint.slice, Name)
+                                ):
                                     value = type_hint.value.name
                                     slice_name = type_hint.slice.name
                                     if value == "Optional":
@@ -54,7 +68,12 @@ def get_instance_attributes(class_node: astroid.ClassDef) -> list[Attribute]:
                                         types.add(value + "[" + slice_name + "]")
                                         remove_types.add(value)
                                         remove_types.add(value.lower())
-                                elif isinstance(type_hint, Subscript) and isinstance(type_hint.value, Name) and isinstance(type_hint.slice, astroid.Tuple) and type_hint.value.name == "Union":
+                                elif (
+                                    isinstance(type_hint, Subscript)
+                                    and isinstance(type_hint.value, Name)
+                                    and isinstance(type_hint.slice, astroid.Tuple)
+                                    and type_hint.value.name == "Union"
+                                ):
                                     for type_name in type_hint.slice.elts:
                                         if isinstance(type_name, Name):
                                             types.add(type_name.name)
