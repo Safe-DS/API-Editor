e8b69e8a7c44b136e9b139b0aec1733de95da912
Aclrian <32142988+Aclrian@users.noreply.github.com>
2023-02-16 07:16:55 +0100

    fix None Error

--- package-parser/package_parser/processing/migration/annotations/_get_migration_text.py
+++ package-parser/package_parser/processing/migration/annotations/_get_migration_text.py
@@ -78,9 +78,9 @@ def _get_further_information(annotation: AbstractAnnotation) -> str:
         if isinstance(annotation, (ConstantAnnotation, OptionalAnnotation)):
             value += (
                 "' and the default Value '"
-                + annotation.defaultValue
+                + str(annotation.defaultValue)
                 + " ( type: "
-                + annotation.defaultValueType.value
+                + str(annotation.defaultValueType.value)
                 + " )"
             )
         value += "'"
