9de24b3b8b4997290f6c5ae7d1c58b397f32aea5
Aclrian <32142988+Aclrian@users.noreply.github.com>
2023-02-16 15:38:21 +0100

    fix build error

--- package-parser/migration_test_data/api/apiv2/library_apiv2/__init__.py
+++ package-parser/migration_test_data/api/apiv2/library_apiv2/__init__.py
@@ -1,5 +1,5 @@
 from .library import Library
 from .media import Book, Media
-from .notificate import notificate
+from .notificate import send_message_to_person
 from .persons import Employee, LibraryMember
 from .position import Position
