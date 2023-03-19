f15f10e82806ce1d12e6bce25ff460e00c7e094c
Lars Reimann <mail@larsreimann.com>
2023-03-19 19:23:53 +0100

    fix: issue URLs

--- api-editor/gui/src/features/externalLinks/urlBuilder.ts
+++ api-editor/gui/src/features/externalLinks/urlBuilder.ts
@@ -1,7 +1,8 @@
 import { jsonCode } from '../../common/util/stringOperations';
 import { Annotation } from '../annotations/versioning/AnnotationStoreV2';
 
-const baseURL = 'https://github.com/lars-reimann/api-editor';
+const apiEditorBaseURL = 'https://github.com/Safe-DS/API-Editor';
+const libraryAnalyzerBaseURL = 'https://github.com/Safe-DS/Library-Analyzer';
 
 // Documentation
 
@@ -11,12 +12,13 @@ export const userGuideURL = `${documentationBaseURL}/gui`;
 
 // Issues
 
-const issueBaseURL = `${baseURL}/issues/new`;
+const apiEditorIssueBaseURL = `${apiEditorBaseURL}/issues/new`;
+const libraryAnalyzerIssueBaseURL = `${libraryAnalyzerBaseURL}/issues/new`;
 
-export const bugReportURL = `${issueBaseURL}?assignees=&labels=bug&template=bug_report.yml`;
-export const featureRequestURL = `${issueBaseURL}?assignees=&labels=enhancement&template=feature_request.yml`;
+export const bugReportURL = `${apiEditorIssueBaseURL}?assignees=&labels=bug&template=bug_report.yml`;
+export const featureRequestURL = `${apiEditorIssueBaseURL}?assignees=&labels=enhancement&template=feature_request.yml`;
 
-const baseMissingAnnotationURL = `${issueBaseURL}?assignees=&labels=bug%2Cmissing+annotation&template=missing_annotation.yml`;
+const baseMissingAnnotationURL = `${libraryAnalyzerIssueBaseURL}?assignees=&labels=bug%2Cmissing+annotation&template=missing_annotation.yml`;
 
 export const missingAnnotationURL = function (target: string): string {
     const urlHash = encodeURIComponent(`\`#/${target}\``);
@@ -24,7 +26,7 @@ export const missingAnnotationURL = function (target: string): string {
     return baseMissingAnnotationURL + `&url-hash=${urlHash}`;
 };
 
-const baseWrongAnnotationURL = `${issueBaseURL}?assignees=&template=wrong_annotation.yml&labels=bug%2Cwrong+annotation%2C`;
+const baseWrongAnnotationURL = `${libraryAnalyzerIssueBaseURL}?assignees=&template=wrong_annotation.yml&labels=bug%2Cwrong+annotation%2C`;
 
 export const wrongAnnotationURL = function (annotationType: string, annotation: Annotation): string {
     const minimalAnnotation = {
