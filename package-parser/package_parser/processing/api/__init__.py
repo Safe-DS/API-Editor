2973c14a7bd07032de5363a0fdcd68c5e166045f
Lars Reimann <mail@larsreimann.com>
2023-03-19 18:15:15 +0100

    chore: remove `package-parser` (#1286)
    Close #1258.
    
    ### Summary of Changes
    
    The `package-parser` is now called `library-analyzer` and resides in its
    [own repo](https://github.com/Safe-DS/Library-Analyzer) for better
    visibility.

--- package-parser/package_parser/processing/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._get_api import get_api
-from ._get_instance_attributes import get_instance_attributes
-from ._get_parameter_list import get_parameter_list
-from ._package_metadata import (
-    distribution,
-    distribution_version,
-    package_files,
-    package_root,
-)
